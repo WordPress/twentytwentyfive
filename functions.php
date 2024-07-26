0b062e9c2268eddb63a5d8a17fae6840584d1176
“Amjad <amjadr360@gmail.com>
2024-07-26 10:10:16 +0500

    add newline

--- functions.php
+++ functions.php
@@ -7,3 +7,11 @@
  * @package Twenty Twenty-Five
  * @since Twenty Twenty-Five 1.0
  */
+// Improperly formatted PHP function.
+function myFunction( $arg1, $arg2 ) {
+	if ( $arg1 === $arg2 ) {
+		echo 'Arguments are equal';
+	} else {
+		echo 'Arguments are not equal';
+	}
+}

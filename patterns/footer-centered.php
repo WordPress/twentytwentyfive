c4edec6557eaf162d7f4d0c900433855af745f3c
Carolina Nymark <hi@themesbycarolina.com>
2024-08-18 08:33:07 +0200

    Add centered footer

---
+++ patterns/footer-centered.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Centered Footer
+ * Slug: twentytwentyfive/footer-centered
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Description: Footer with centered site title and tagline
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:site-title {"level":0,"textAlign":"center"} /-->
+	<!-- wp:site-tagline {"textAlign":"center","fontSize":"small"} /-->
+	<!-- wp:spacer {"height":"20px"} -->
+	<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+	<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+	<p class="has-text-align-center has-small-font-size">Designed with <strong>WordPress</strong></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->

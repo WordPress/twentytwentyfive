9f2c5505d20c065f99cc44a801c4d42bce361bde
Carolina Nymark <hi@themesbycarolina.com>
2024-08-20 12:37:09 +0200

    Update header.php

--- patterns/header.php
+++ patterns/header.php
@@ -5,7 +5,12 @@
  * Categories: header
  * Block Types: core/template-part/header
  * Description: Header with site title and navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
  */
+
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--50)">

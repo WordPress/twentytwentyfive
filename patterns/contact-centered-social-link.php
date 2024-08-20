1e12966472c5383483a6a5f223caaef2b4681571
Kishan Jasani <kishanjasani007@yahoo.in>
2024-08-20 22:36:18 +0530

    Fix build error

--- patterns/contact-centered-social-link.php
+++ patterns/contact-centered-social-link.php
@@ -4,13 +4,16 @@
  * Slug: twentytwentyfive/contact-centered-social-link
  * Keywords: contact, social-link
  * Categories: contact
+ *
+ * @package Twenty Twenty-Five
  */
+
 ?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
 	<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontSize":"x-large"} -->
-	<p class="has-text-align-center has-contrast-color has-text-color has-link-color has-x-large-font-size">Got questions?<br>Feel free to reach out.</p>
+	<p class="has-text-align-center has-contrast-color has-text-color has-link-color has-x-large-font-size"><?php echo esc_html_x( 'Got questions? Feel free to reach out.', 'Heading of the Contact social link pattern', 'twentytwentyfive' ); ?></p>
 	<!-- /wp:paragraph -->
 
 	<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#111111","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->

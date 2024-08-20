320d10b67f2da853925008f9ff408fee0752c031
Carolina Nymark <hi@themesbycarolina.com>
2024-08-20 14:40:37 +0200

    Override the global left and right padding on the inner group block

--- patterns/header-large-title.php
+++ patterns/header-large-title.php
@@ -15,8 +15,8 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--50)">
 	<!-- wp:site-title {"level":0,"style":{"typography":{"fontSize":"100px"}}} /-->
-	<!-- wp:group {"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
+	<!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="padding-right:0;padding-left:0">
 		<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"right","orientation":"vertical"}} /-->
 	</div>
 	<!-- /wp:group -->

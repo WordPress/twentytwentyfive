d2970d9268d021745c6a380ede47b4d0c8a8e424
Carolina Nymark <hi@themesbycarolina.com>
2024-08-20 13:35:33 +0200

    Try link and focus styles

--- functions.php
+++ functions.php
@@ -7,3 +7,23 @@
  * @package Twenty Twenty-Five
  * @since Twenty Twenty-Five 1.0
  */
+
+/**
+ * Enqueue stylesheet.css in the editors.
+ */
+ function twenty_twenty_five_editor_style() {
+	add_editor_style( get_parent_theme_file_uri( 'style.css' ) );
+}
+add_action( 'after_setup_theme', 'twenty_twenty_five_editor_style' );
+
+/**
+ * Enqueue stylesheet.css on the front.
+ */
+function twenty_twenty_five_enqueue_styles() {
+    wp_enqueue_style(
+        'twenty-twenty-five-style',
+        get_parent_theme_file_uri( 'style.css' ),
+        array(),
+        wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'twenty_twenty_five_enqueue_styles' );

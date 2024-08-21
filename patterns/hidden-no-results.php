18f698b5d54dcf852ebe6d0eb492d6e4ef985551
Carolina Nymark <hi@themesbycarolina.com>
2024-08-21 14:44:29 +0200

    Fix spacing in the no results pattern

--- patterns/hidden-no-results.php
+++ patterns/hidden-no-results.php
@@ -9,7 +9,7 @@
  * @since Twenty Twenty-Five 1.0
  */
 
- ?>
+?>
 <!-- wp:paragraph -->
 <p><?php echo esc_html_x( 'No posts were found.', 'Message explaining that there are no results returned from a search', 'twentytwentyfive' ); ?></p>
 <!-- /wp:paragraph -->

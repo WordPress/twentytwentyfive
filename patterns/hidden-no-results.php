3f2890cfe34fc295ce771bc94ced55dce8793cf9
Carolina Nymark <hi@themesbycarolina.com>
2024-08-21 14:41:07 +0200

    Add templates and theme.json changes for "personal blog"

---
+++ patterns/hidden-no-results.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: No results
+ * Slug: twentytwentyfive/hidden-no-results
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+ ?>
+<!-- wp:paragraph -->
+<p><?php echo esc_html_x( 'No posts were found.', 'Message explaining that there are no results returned from a search', 'twentytwentyfive' ); ?></p>
+<!-- /wp:paragraph -->

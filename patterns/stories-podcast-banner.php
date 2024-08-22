2235a27a83e32703658974e6f5d9a4738cfc398a
Sam Toohey <hello@samtoohey.xyz>
2024-08-22 17:26:08 +1000

    Hero Image element
    Adding In stories hero element for the podcast banner
    
    One note is I pulled the image from figma, wasnt sure where to get the raw file so would love to try to get that real image and resolve that in a PR later
    
    pls LMK feedback :D

---
+++ patterns/stories-podcast-banner.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Title: Stories Podcast Hero
+ * Slug: twentytwentyfive/stories-podcast-hero
+ * Categories: featured, hero
+ * Keywords: podcast, hero, stories
+ */
+?>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70","right":"var:preset|spacing|70"}},"color":{"background":"#F6CFF4"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-background" style="background-color:#F6CFF4;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"40%","layout":{"type":"default"}} -->
+		<div class="wp-block-column" style="flex-basis:40%"><!-- wp:image {"className":"wp-block-image size-large is-resized"} -->
+			<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/image-from-rawpixel-id-14264288-jpeg.png" alt="Historical figure" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"48px"}}} -->
+			<h1 class="wp-block-heading" style="font-size:48px">The Stories® Podcast</h1>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}}} -->
+			<p style="font-size:22px">Storytelling, expert analysis, and vivid descriptions. The Stories® Podcast brings history to life, making it accessible and engaging for a global audience.</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontWeight":"700","fontSize":"22px"}}} -->
+			<p style="font-size:22px;font-weight:700">Subscribe on your favorite platform</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+			<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Youtube</a></div>
+				<!-- /wp:button -->
+
+				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Apple Podcasts</a></div>
+				<!-- /wp:button -->
+
+				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Spotify</a></div>
+				<!-- /wp:button -->
+
+				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Pocket Casts</a></div>
+				<!-- /wp:button -->
+
+				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">RSS</a></div>
+				<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->

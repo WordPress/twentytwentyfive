2702493194b7778607d077a09197c8bfced1da93
Carolina Nymark <hi@themesbycarolina.com>
2024-08-22 15:40:31 +0200

    Update patterns/footer-newsletter.php
    Co-authored-by: Juan Aldasoro <juanfraa@gmail.com>

--- patterns/footer-newsletter.php
+++ patterns/footer-newsletter.php
@@ -34,7 +34,7 @@
 	<div class="wp-block-group alignfull">
 		<!-- wp:site-title {"level":0,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"x-small"} /-->
 		<!-- wp:paragraph {"fontSize":"x-small"} -->
-		<p class="has-small-font-size">Designed with <strong>WordPress</strong></p>
+		<p class="has-x-small-font-size">Designed with <strong>WordPress</strong></p>
 		<!-- /wp:paragraph -->
 	</div>
 	<!-- /wp:group -->

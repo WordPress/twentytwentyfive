92a7e095afd20ccf834d2747df7c5106511f71d7
Alessio Ruggieri <alessio.ruggieri90@gmail.com>
2024-08-23 17:29:07 +0200

    change some designe issues

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -11,26 +11,26 @@
 
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":{"left":"var:preset|spacing|70"}}}} -->
-	<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}}} -->
+	<div class="wp-block-columns alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
 		<!-- wp:column {"width":"40%"} -->
 		<div class="wp-block-column" style="flex-basis:40%">
-			<!-- wp:image {"aspectRatio":"2/3","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+			<!-- wp:image {"sizeSlug":"large","linkDestination":"none","className":"is-resized"} -->
 			<figure class="wp-block-image size-large is-resized">
-				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-13029755-jpeg.webp" alt="<?php echo esc_attr_x( 'Small totara tree on ridge above Long Point', 'image description', 'twentytwentyfive' ); ?>" class="" style="aspect-ratio:2/3;object-fit:cover"/>
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-13029755-jpeg.webp" alt="<?php echo esc_attr_x( 'Small totara tree on ridge above Long Point', 'image description', 'twentytwentyfive' ); ?>"/>
 			</figure>
 			<!-- /wp:image -->
 		</div>
 		<!-- /wp:column -->
 
-		<!-- wp:column {"verticalAlignment":"bottom","width":"60%"} -->
-		<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:60%">
-			<!-- wp:heading {"level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"x-large"} -->
-			<h1 class="wp-block-heading has-x-large-font-size" style="font-style:normal;font-weight:500">
+		<!-- wp:column {"verticalAlignment":"bottom","width":"60%","style":{"spacing":{"padding":{"left":"var:preset|spacing|40"}}}} -->
+		<div class="wp-block-column is-vertically-aligned-bottom" style="padding-left:var(--wp--preset--spacing--40);flex-basis:60%">
+			<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} -->
+			<h3 class="wp-block-heading" style="font-style:normal;font-weight:500">
 				<?php echo esc_html_x( 'Page not found', '404 error message', 'twentytwentyfive' ); ?>
-			</h1>
+			</h3>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->

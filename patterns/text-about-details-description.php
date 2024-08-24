15436d0e73966bc1fa5fdd131552fd9371b07dac
shail-mehta <shailmehta25@gmail.com>
2024-08-24 14:03:03 +0530

    Fix phpcs issue

--- patterns/text-about-details-description.php
+++ patterns/text-about-details-description.php
@@ -9,31 +9,32 @@
  * @subpackage Twenty_Twenty_Five
  * @since Twenty Twenty-Five 1.0
  */
-
 ?>
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70","right":"var:preset|spacing|70"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":""}} -->
 <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
     <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|70","left":"var:preset|spacing|70"}}}} -->
     <div class="wp-block-columns alignwide">
-        <!-- wp:column {"verticalAlignment":"top","width":"45%"} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="flex-basis:45%">
+        <!-- wp:column {"verticalAlignment":"top","width":"50%"} -->
+        <div class="wp-block-column is-vertically-aligned-top" style="flex-basis:50%">
             <!-- wp:heading {"level":3,"fontSize":"x-large"} -->
-            <h3 class="wp-block-heading has-x-large-font-size"><?php esc_html_e('Let’s hear about history and ancestry', 'twentytwentyfive'); ?></h3>
+            <h3 class="wp-block-heading has-x-large-font-size">
+                <?php esc_html_e('Let’s hear about history and ancestry', 'twentytwentyfive'); ?>
+            </h3>
             <!-- /wp:heading -->
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
-        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%">
-
+        <!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%">
             <!-- wp:paragraph {"style":{"typography":{"fontSize":"small","textTransform":"uppercase"}}} -->
-			<p class="has-primary-color has-small-font-size" style="text-transform:uppercase"><?php esc_html_e('What will happen at “Stories, historias, iсторії, iστορίες”:', 'twentytwentyfive'); ?></p>
-			<!-- /wp:paragraph -->
+            <p class="has-primary-color has-small-font-size" style="text-transform:uppercase">
+                <?php esc_html_e('What will happen at “Stories, historias, iсторії, iστορίες”:', 'twentytwentyfive'); ?>
+            </p>
+            <!-- /wp:paragraph -->
 
             <!-- wp:list {"fontSize":"small"} -->
             <ul class="wp-block-list has-small-font-size">
-
                 <!-- wp:list-item -->
                 <li><?php esc_html_e('Opportunity to learn from master photographers and historians', 'twentytwentyfive'); ?></li>
                 <!-- /wp:list-item -->
@@ -44,7 +45,6 @@
 
                 <!-- wp:list-item -->
                 <li><?php esc_html_e('Participants engage in hands-on activities that not only enhance their technical skills but also deepen their understanding of the cultural and historical contexts of the photographs', 'twentytwentyfive'); ?></li>
-
                 <!-- /wp:list-item -->
 
                 <!-- wp:list-item -->
@@ -54,7 +54,6 @@
                 <!-- wp:list-item -->
                 <li><?php esc_html_e('Open conversations that delve into how photography can bridge cultural divides', 'twentytwentyfive'); ?></li>
                 <!-- /wp:list-item -->
-
             </ul>
             <!-- /wp:list -->
         </div>

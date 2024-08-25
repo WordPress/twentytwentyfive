531cb3adda9273d82f589bbdc08383c7c32135c2
Sam Toohey <hello@samtoohey.xyz>
2024-08-25 22:17:54 +1000

    Version 2 of Podcast Hero element
    Actioning all feedback based off of #105
    
    Some things on the docs I wasnt sure on but would love more feedback @juanfra

--- patterns/stories-podcast-banner.php
+++ patterns/stories-podcast-banner.php
@@ -1,54 +1,63 @@
 <?php
-
 /**
  * Title: Stories Podcast Hero
  * Slug: twentytwentyfive/stories-podcast-hero
  * Categories: featured, hero
  * Keywords: podcast, hero, stories
+ *
+ * @package TwentyTwentyFive
  */
+
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70","right":"var:preset|spacing|70"}},"color":{"background":"#F6CFF4"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-background" style="background-color:#F6CFF4;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
-	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"40%","layout":{"type":"default"}} -->
-		<div class="wp-block-column" style="flex-basis:40%"><!-- wp:image {"className":"wp-block-image size-large is-resized"} -->
-			<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/image-from-rawpixel-id-14264288-jpeg.png" alt="Historical figure" /></figure>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}},"backgroundColor":"accent-2"},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull has-accent-2-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+		<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"width":"40%"} -->
+		<div class="wp-block-column" style="flex-basis:40%">
+			<!-- wp:image {"width":"360px","height":"360px","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"20px"}}} -->
+			<figure class="wp-block-image size-large is-resized" style="border-radius:20px">
+				<img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/image-from-rawpixel-id-14264288-jpeg.webp' ) ); ?>" alt="Historical figure" style="width:360px;height:360px;object-fit:cover" />
+			</figure>
 			<!-- /wp:image -->
 		</div>
 		<!-- /wp:column -->
 
 		<!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
-		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"48px"}}} -->
-			<h1 class="wp-block-heading" style="font-size:48px">The Stories® Podcast</h1>
+		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%">
+			<!-- wp:heading {"level":1,"fontSize":"xxx-large"} -->
+			<h2 class="wp-block-heading has-xxx-large-font-size">The Stories® Podcast</h2>
 			<!-- /wp:heading -->
 
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}}} -->
-			<p style="font-size:22px">Storytelling, expert analysis, and vivid descriptions. The Stories® Podcast brings history to life, making it accessible and engaging for a global audience.</p>
-			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"fontSize":"medium"} -->
+			<p class="has-medium-font-size">Storytelling, expert analysis, and vivid descriptions. The Stories® Podcast brings history to life, making it accessible and engaging for a global audience.</p>
+				<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph {"style":{"typography":{"fontWeight":"700","fontSize":"22px"}}} -->
-			<p style="font-size:22px;font-weight:700">Subscribe on your favorite platform</p>
-			<!-- /wp:paragraph -->
+			<!-- wp:heading {"level":6} -->
+			<h6 class="wp-block-heading">Subscribe on your favorite platform</h6>
+			<!-- /wp:heading -->
 
 			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
-			<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Youtube</a></div>
+			<div class="wp-block-buttons">
+				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small","size":"extra-small"} -->
+				<div class="wp-block-button has-extra-small-font-size is-style-outline"><a class="wp-block-button__link wp-element-button">Youtube</a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Apple Podcasts</a></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small","size":"extra-small"} -->
+				<div class="wp-block-button has-extra-small-font-size is-style-outline"><a class="wp-block-button__link wp-element-button">Apple Podcasts</a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Spotify</a></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small","size":"extra-small"} -->
+				<div class="wp-block-button has-extra-small-font-size is-style-outline"><a class="wp-block-button__link wp-element-button">Spotify</a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">Pocket Casts</a></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small","size":"extra-small"} -->
+				<div class="wp-block-button has-extra-small-font-size is-style-outline"><a class="wp-block-button__link wp-element-button">Pocket Casts</a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"}},"typography":{"fontSize":"12px"}}} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:4px;padding-right:8px;padding-bottom:4px;padding-left:8px">RSS</a></div>
+					<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small","size":"extra-small"} -->
+				<div class="wp-block-button has-extra-small-font-size is-style-outline"><a class="wp-block-button__link wp-element-button">RSS</a></div>
 				<!-- /wp:button -->
 			</div>
 			<!-- /wp:buttons -->

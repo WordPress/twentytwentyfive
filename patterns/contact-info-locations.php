e8acd2421377fc76d5edeb77bf741a4d7155d6f0
Carolina Nymark <hi@themesbycarolina.com>
2024-08-26 14:33:26 +0200

    Add pattern: Contact: Info and locations

---
+++ patterns/contact-info-locations.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Title: Contact: Info and locations
+ * Slug: twentytwentyfive/contact-info-locations
+ * Keywords: contact, location
+ * Categories: contact
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:heading {"textAlign":"left","align":"full","fontSize":"xx-large"} -->
+	<h2 class="wp-block-heading alignfull has-text-align-left has-xx-large-font-size">How to get in touch with us</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"blockGap":"0","margin":{"top":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|primary","width":"1px"},"right":{},"bottom":{},"left":{}}},"layout":{"type":"grid","minimumColumnWidth":"23rem"}} -->
+	<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--primary);border-top-width:1px;margin-top:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+		<!-- wp:group {"style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group">
+			<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+			<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">Social media</h3>
+			<!-- /wp:heading -->
+			<!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"0"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical"},"ariaLabel":"<?php esc_attr_e( 'Social', 'twentytwentyfive' ); ?>"} -->
+				<!-- wp:navigation-link {"label":"<?php esc_html_e( 'X/Twitter', 'twentytwentyfive' ); ?>","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Instagram', 'twentytwentyfive' ); ?>","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Facebook', 'twentytwentyfive' ); ?>","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"<?php esc_html_e( 'TikTok', 'twentytwentyfive' ); ?>","url":"#"} /-->
+			<!-- /wp:navigation -->
+			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+			<h2 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">Email:</h2>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size">Example@example.com</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:group {"layout":{"type":"grid","minimumColumnWidth":null,"columnCount":"2"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+				<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">New York</h3>
+				<!-- /wp:heading -->
+				<!-- wp:paragraph {"fontSize":"small"} -->
+				<p class="has-small-font-size">123 Example St.Manhattan, NY 10300 United States</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+				<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">New York</h3>
+				<!-- /wp:heading -->
+				<!-- wp:paragraph {"fontSize":"small"} -->
+				<p class="has-small-font-size">123 Example St.Manhattan, NY 10300 United States</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+				<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">New York</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"fontSize":"small"} -->
+				<p class="has-small-font-size">123 Example St.Manhattan, NY 10300 United States</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+				<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">New York</h3>
+				<!-- /wp:heading -->
+				<!-- wp:paragraph {"fontSize":"small"} -->
+				<p class="has-small-font-size">123 Example St.Manhattan, NY 10300 United States</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->

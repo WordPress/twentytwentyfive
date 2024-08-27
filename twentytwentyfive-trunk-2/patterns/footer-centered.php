d600e80f929b34c1c84afb3d5481f2dfa6ab5318
Beatriz Fialho <65220155+beafialho@users.noreply.github.com>
2024-08-27 17:24:56 +0100

    Adds alternative Personal Blog templates listed in #132
    Also exported as a zip file.
    
    * [Preview loaded from this PR – available **before** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FWordPress%2Ftwentytwentyfive%2Fplayground-changes-2024-08-27T16-23-10-157Z%2Ftwentytwentyfive-trunk-2%2Fplayground.zip)
    * [Preview loaded from the main branch – available **after** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FWordPress%2Ftwentytwentyfive%2Ftrunk%2Ftwentytwentyfive-trunk-2%2Fplayground.zip)

---
+++ twentytwentyfive-trunk-2/patterns/footer-centered.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Centered Footer
+ * Slug: twentytwentyfive/footer-centered
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Description: Footer with centered site title and tagline
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:site-title {"level":0,"textAlign":"center"} /-->
+	<!-- wp:site-tagline {"textAlign":"center"} /-->
+	<!-- wp:spacer {"height":"20px"} -->
+	<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+	<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+	<p class="has-text-align-center has-x-small-font-size">Designed with <strong>WordPress</strong></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->

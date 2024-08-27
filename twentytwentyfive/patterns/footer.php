27e9ba29f44579d278b1e5c0a0bfe6f9f57cbfb6
Beatriz Fialho <65220155+beafialho@users.noreply.github.com>
2024-08-27 18:27:20 +0100

    Adds alternative Personal Blog templates listed in #132
    Also exported as a zip file.
    
    * [Preview loaded from this PR – available **before** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FWordPress%2Ftwentytwentyfive%2Fplayground-changes-2024-08-27T17-22-09-252Z%2Ftwentytwentyfive%2Fplayground.zip)
    * [Preview loaded from the main branch – available **after** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FWordPress%2Ftwentytwentyfive%2Ftrunk%2Ftwentytwentyfive%2Fplayground.zip)

---
+++ twentytwentyfive/patterns/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: footer
+ * Slug: twentytwentyfive/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"metadata":{"categories":["footer"],"patternName":"twentytwentyfive/footer-centered","name":"Centered Footer"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:site-title {"level":0,"textAlign":"center"} /-->
+
+<!-- wp:site-tagline {"textAlign":"center"} /-->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php esc_html_e('Designed with <strong>WordPress</strong>', 'twentytwentyfive');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->

82443a7758ae447b21e44809c9e29e16e6f91f75
Carolina Nymark <hi@themesbycarolina.com>
2024-08-28 13:04:08 +0200

    Try moving the text only template files to patterns

---
+++ patterns/template-text-only-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Text only home template
+ * Slug: twentytwentyfive/template-text-only-home
+ * Template Types: front-page, home
+ * Viewport width: 1400
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+
+<!-- wp:pattern {"slug":"twentytwentyfive/posts-text-only-blog"} /-->

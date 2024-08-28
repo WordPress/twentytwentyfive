fa715f1d012a8dd801dfed472958cb48510da5da
Carolina Nymark <hi@themesbycarolina.com>
2024-08-28 09:49:50 +0200

    Update pattern markup
    - Change width from full to wide and remove spacing.
    - Adjust the column span of the left column to make it wider.
    - Fix a heading level that was incorrect: from H2 to H3.

--- patterns/contact-info-locations.php
+++ patterns/contact-info-locations.php
@@ -11,15 +11,15 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);">
 	<!-- wp:heading {"textAlign":"left","align":"full","fontSize":"xx-large"} -->
 	<h2 class="wp-block-heading alignfull has-text-align-left has-xx-large-font-size">How to get in touch with us</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"blockGap":"0","margin":{"top":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|primary","width":"1px"},"right":{},"bottom":{},"left":{}}},"layout":{"type":"grid","minimumColumnWidth":"23rem"}} -->
 	<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--primary);border-top-width:1px;margin-top:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:group {"style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical"}} -->
+		<!-- wp:group {"style":{"layout":{"rowSpan":1,"columnSpan":"2"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 		<div class="wp-block-group">
 			<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
 			<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">Social media</h3>
@@ -30,8 +30,8 @@
 				<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Facebook', 'twentytwentyfive' ); ?>","url":"#"} /-->
 				<!-- wp:navigation-link {"label":"<?php esc_html_e( 'TikTok', 'twentytwentyfive' ); ?>","url":"#"} /-->
 			<!-- /wp:navigation -->
-			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
-			<h2 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">Email:</h2>
+			<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+			<h3 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">Email:</h2>
 			<!-- /wp:heading -->
 			<!-- wp:paragraph {"fontSize":"small"} -->
 			<p class="has-small-font-size">example@example.com</p>

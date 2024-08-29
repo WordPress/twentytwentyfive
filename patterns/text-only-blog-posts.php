124ad6d85e5eb39b4e432d3eb6985c583eb0df8d
Carolina Nymark <hi@themesbycarolina.com>
2024-08-29 06:32:37 +0200

    Update text-only-blog-posts.php

--- patterns/text-only-blog-posts.php
+++ patterns/text-only-blog-posts.php
@@ -39,7 +39,7 @@
 
 	<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);">
-		<!-- wp:query-pagination {"align":"wide","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
+		<!-- wp:query-pagination {"align":"full","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
 		<!-- wp:query-pagination-previous /-->
 		<!-- wp:query-pagination-numbers /-->
 		<!-- wp:query-pagination-next /-->

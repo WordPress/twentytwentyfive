f89607de52e55c6683998b25994ac960f657e494
jasmussen <joen@automattic.com>
2024-08-30 09:47:47 +0200

    Remaining x-small to small conversions.

--- patterns/footer.php
+++ patterns/footer.php
@@ -52,7 +52,7 @@
 		<div class="wp-block-group alignfull">
 			<!-- wp:site-title {"level":0,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} /-->
 			<!-- wp:paragraph {"fontSize":"small"} -->
-			<p class="has-x-small-font-size">Designed with <strong>WordPress</strong></p>
+			<p class="has-small-font-size">Designed with <strong>WordPress</strong></p>
 			<!-- /wp:paragraph -->
 		</div>
 		<!-- /wp:group -->

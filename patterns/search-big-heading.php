fe20ef4d532c3bbfb7f8ab1cdf560e264882f56a
Jennifer Farhat <jennifer.farhat@webdevstudios.com>
2024-08-30 12:38:02 -0700

    Big heading and search form

---
+++ patterns/search-big-heading.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Search - Big heading and search form
+ * Slug: twentytwentyfive/search-big-heading
+ * Categories: call-to-action, text
+ * Description: heading with search form
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Big heading and search form"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"140px","bottom":"140px"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignwide" style="padding-top:140px;padding-bottom:140px"><!-- wp:heading {"level":1} -->
+	<h1 class="wp-block-heading"><?php esc_html_e( 'What are you looking for?', 'twentytwentyfive' ); ?></h1>
+	<!-- /wp:heading -->
+
+	<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"<?php esc_html_e( 'Type here...', 'twentytwentyfive' ); ?>","width":100,"widthUnit":"%","buttonText":"Search"} /-->
+</div>
+<!-- /wp:group -->

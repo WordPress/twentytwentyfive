97b5f40a625dda220446f9e1fd259b1b330880dc
Juan Aldasoro <juanfraa@gmail.com>
2024-08-30 18:00:47 +0200

    Indent sidebar template.

--- patterns/hidden-sidebar.php
+++ patterns/hidden-sidebar.php
@@ -20,21 +20,25 @@
 <!-- /wp:spacer -->
 
 <!-- wp:query {"queryId":12,"query":{"perPage":4,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]}} -->
-<div class="wp-block-query"><!-- wp:post-template -->
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:post-title {"isLink":true,"fontSize":"medium"} /-->
+<div class="wp-block-query">
+	<!-- wp:post-template -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group">
+			<!-- wp:post-title {"isLink":true,"fontSize":"medium"} /-->
 
-<!-- wp:post-date {"fontSize":"small"} /--></div>
-<!-- /wp:group -->
+			<!-- wp:post-date {"fontSize":"small"} /-->
+		</div>
+		<!-- /wp:group -->
 
-<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
-<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-<!-- /wp:post-template -->
+		<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+		<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+	<!-- /wp:post-template -->
 
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results.","fontSize":"medium"} -->
-<p class="has-medium-font-size"><?php esc_html_e( 'No posts were found.', 'twentytwentyfive' ); ?></p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
+	<!-- wp:query-no-results -->
+		<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results.","fontSize":"medium"} -->
+		<p class="has-medium-font-size"><?php esc_html_e( 'No posts were found.', 'twentytwentyfive' ); ?></p>
+		<!-- /wp:paragraph -->
+	<!-- /wp:query-no-results -->
+</div>
 <!-- /wp:query -->

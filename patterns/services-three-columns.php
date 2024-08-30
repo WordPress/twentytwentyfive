3f1d17cb080996e624ff38bf9b926d4b16857300
Joen A. <1204802+jasmussen@users.noreply.github.com>
2024-08-30 10:19:04 +0200

    Update patterns/services-three-columns.php
    Co-authored-by: Carolina Nymark <hi@themesbycarolina.com>

--- patterns/services-three-columns.php
+++ patterns/services-three-columns.php
@@ -50,7 +50,7 @@
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph {"fontSize":"medium"} -->
-			<p class="has-small-font-size">Like flowers that bloom in unexpected places, every story unfolds with beauty and resilience</p>
+			<p class="has-medium-font-size">Like flowers that bloom in unexpected places, every story unfolds with beauty and resilience</p>
 			<!-- /wp:paragraph -->
 		</div>
 		<!-- /wp:column -->

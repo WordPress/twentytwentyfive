f1871da553af89483fb2928421527af39e803079
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-08-31 18:11:59 +0600

    add Services - Three-column boxed pricing table

---
+++ patterns/Services - Three-column boxed pricing table.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Title: Services - Three-column boxed pricing table
+ * Slug: twentytwentyfive/services-three-column-boxed-pricing-table
+ * Categories: call-to-action, banner, featured, services, pricing
+ * Description: A three-column boxed pricing table designed to showcase services, descriptions, and pricing options.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+
+
+<!-- wp:group {"metadata":{"name":"Services - Three-column boxed pricing table"},"align":"full","style":{"spacing":{"padding":{"top":"69px","bottom":"69px","left":"50px","right":"50px"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:69px;padding-right:50px;padding-bottom:69px;padding-left:50px"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading -->
+<h2 class="wp-block-heading">Choose your membership</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"contrast","className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"12px"},"border":{"style":"solid","width":"1px"}},"fontFamily":"manrope","borderColor":"contrast"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline has-manrope-font-family" style="font-size:12px"><a class="wp-block-button__link has-contrast-color has-text-color has-link-color has-border-color has-contrast-border-color wp-element-button" style="border-style:solid;border-width:1px" rel="#">Become a member</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"97px","bottom":"0"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:97px;margin-bottom:0"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"1px","color":"#11111133","radius":"10px"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"30px"}}}} -->
+<div class="wp-block-group has-border-color" style="border-color:#11111133;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:30px"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"}}} -->
+<div class="wp-block-column" style="font-size:22px;font-style:normal;font-weight:400;line-height:1.8"><!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="font-size:22px;font-style:normal;font-weight:400;line-height:1.8">Free</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:14px;font-style:normal;font-weight:400;line-height:1.8">You get access to our free articles and weekly newsletter.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<div class="wp-block-column" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:14px;font-style:normal;font-weight:400;line-height:1.8"><!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"38px","fontStyle":"normal","fontWeight":"400","lineHeight":"2.6","letterSpacing":"0.08px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-text-align-right has-contrast-color has-text-color has-link-color" style="font-size:38px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:2.6">0€</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"400","letterSpacing":"0.08px","lineHeight":"1"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"margin":{"top":"-20px"}}},"textColor":"primary"} -->
+<p class="has-text-align-right has-primary-color has-text-color has-link-color" style="margin-top:-20px;font-size:12px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:1">Month</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"21px","bottom":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:21px;margin-bottom:0"><!-- wp:column {"style":{"typography":{"fontSize":"38px","fontStyle":"normal","fontWeight":"400","lineHeight":"2.6","letterSpacing":"0.08px"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary"} -->
+<div class="wp-block-column has-primary-color has-text-color has-link-color" style="font-size:38px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:2.6"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100,"style":{"typography":{"fontSize":"18px","lineHeight":"1.2","letterSpacing":"0.08px"},"spacing":{"padding":{"left":"36px","right":"36px","top":"16px","bottom":"16px"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size" style="font-size:18px;letter-spacing:0.08px;line-height:1.2"><a class="wp-block-button__link wp-element-button" style="padding-top:16px;padding-right:36px;padding-bottom:16px;padding-left:36px" rel="#">Join</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"1px","color":"#11111133","radius":"10px"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"30px"}}}} -->
+<div class="wp-block-group has-border-color" style="border-color:#11111133;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:30px"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"}}} -->
+<div class="wp-block-column" style="font-size:22px;font-style:normal;font-weight:400;line-height:1.8"><!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="font-size:22px;font-style:normal;font-weight:400;line-height:1.8">Single</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:14px;font-style:normal;font-weight:400;line-height:1.8">Get access to our paid newsletter  and a limited pass  for one IRL  event.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<div class="wp-block-column" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:14px;font-style:normal;font-weight:400;line-height:1.8"><!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"38px","fontStyle":"normal","fontWeight":"400","lineHeight":"2.6","letterSpacing":"0.08px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-text-align-right has-contrast-color has-text-color has-link-color" style="font-size:38px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:2.6">20€</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"400","letterSpacing":"0.08px","lineHeight":"1"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"margin":{"top":"-20px"}}},"textColor":"primary"} -->
+<p class="has-text-align-right has-primary-color has-text-color has-link-color" style="margin-top:-20px;font-size:12px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:1">Month</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"21px","bottom":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:21px;margin-bottom:0"><!-- wp:column {"style":{"typography":{"fontSize":"38px","fontStyle":"normal","fontWeight":"400","lineHeight":"2.6","letterSpacing":"0.08px"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary"} -->
+<div class="wp-block-column has-primary-color has-text-color has-link-color" style="font-size:38px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:2.6"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100,"style":{"typography":{"fontSize":"18px","lineHeight":"1.2","letterSpacing":"0.08px"},"spacing":{"padding":{"left":"36px","right":"36px","top":"16px","bottom":"16px"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size" style="font-size:18px;letter-spacing:0.08px;line-height:1.2"><a class="wp-block-button__link wp-element-button" style="padding-top:16px;padding-right:36px;padding-bottom:16px;padding-left:36px" rel="#">Join</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"1px","color":"#11111133","radius":"10px"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"30px"}}}} -->
+<div class="wp-block-group has-border-color" style="border-color:#11111133;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:30px"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"}}} -->
+<div class="wp-block-column" style="font-size:22px;font-style:normal;font-weight:400;line-height:1.8"><!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="font-size:22px;font-style:normal;font-weight:400;line-height:1.8">Expert</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:14px;font-style:normal;font-weight:400;line-height:1.8">Get access to our paid newsletter  and an unlimited event pass.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<div class="wp-block-column" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:14px;font-style:normal;font-weight:400;line-height:1.8"><!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"38px","fontStyle":"normal","fontWeight":"400","lineHeight":"2.6","letterSpacing":"0.08px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<p class="has-text-align-right has-contrast-color has-text-color has-link-color" style="font-size:38px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:2.6">40€</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"400","letterSpacing":"0.08px","lineHeight":"1"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"margin":{"top":"-20px"}}},"textColor":"primary"} -->
+<p class="has-text-align-right has-primary-color has-text-color has-link-color" style="margin-top:-20px;font-size:12px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:1">Month</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"21px","bottom":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:21px;margin-bottom:0"><!-- wp:column {"style":{"typography":{"fontSize":"38px","fontStyle":"normal","fontWeight":"400","lineHeight":"2.6","letterSpacing":"0.08px"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary"} -->
+<div class="wp-block-column has-primary-color has-text-color has-link-color" style="font-size:38px;font-style:normal;font-weight:400;letter-spacing:0.08px;line-height:2.6"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100,"style":{"typography":{"fontSize":"18px","lineHeight":"1.2","letterSpacing":"0.08px"},"spacing":{"padding":{"left":"36px","right":"36px","top":"16px","bottom":"16px"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size" style="font-size:18px;letter-spacing:0.08px;line-height:1.2"><a class="wp-block-button__link wp-element-button" style="padding-top:16px;padding-right:36px;padding-bottom:16px;padding-left:36px" rel="">Join</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->

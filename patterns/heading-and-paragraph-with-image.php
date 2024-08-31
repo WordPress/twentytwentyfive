e1e1692f2c48489c7904e3ab93df80cc4e3f135f
shail-mehta <shailmehta25@gmail.com>
2024-08-31 12:00:24 +0530

    fix div issue

--- patterns/heading-and-paragraph-with-image.php
+++ patterns/heading-and-paragraph-with-image.php
@@ -23,15 +23,16 @@
 			</h3>
 			<!-- /wp:heading -->
 
-			<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
-			<p class="has-base-color has-text-color has-small-font-size">
+			<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"medium"} -->
+			<p class="has-base-color has-text-color has-medium-font-size">
 				<?php echo esc_html_x( 'Held over a weekend, the event is structured around a series of exhibitions, workshops, and panel discussions. The exhibitions showcase a curated selection of photographs that tell compelling stories from various corners of the globe, each image accompanied by detailed narratives that provide context and deeper insight into the historical significance of the scenes depicted. These photographs are drawn from the archives of renowned photographers, as well as emerging talents, ensuring a blend of both classical and contemporary perspectives.', 'Event Overview Text', 'twentytwentyfive' ); ?>
 			</p>
 			<!-- /wp:paragraph -->
 		</div>
 		<!-- /wp:column -->
 
-	   <!-- wp:column {"verticalAlignment":"center","width":"50%","layout":{"type":"default"}} -->
+		<!-- wp:column {"verticalAlignment":"center","width":"50%","layout":{"type":"default"}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"> 
 			<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full"} -->
 			<figure class="wp-block-image size-full">
 				<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/image-from-rawpixel-id-8803077-original.webp' ); ?>" alt="<?php echo esc_attr_x( 'A classic black and white photo of an old church', 'Alt text for Overview picture', 'twentytwentyfive' ); ?>" style="aspect-ratio:1;object-fit:cover" />

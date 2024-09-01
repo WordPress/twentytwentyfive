f26fdb4ce3efd52f35bcb8340ff42b1631491e07
Carolina Nymark <hi@themesbycarolina.com>
2024-09-01 16:42:06 +0200

    Change the post title to H3

--- patterns/more-posts.php
+++ patterns/more-posts.php
@@ -23,7 +23,7 @@
 		<!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
 			<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"border":{"bottom":{"color":"var:preset|color|opacity-20","width":"1px"},"top":[],"right":[],"left":[]}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center","justifyContent":"space-between"}} -->
 			<div class="wp-block-group alignfull" style="border-bottom-color:var(--wp--preset--color--opacity-20);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
-				<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
+				<!-- wp:post-title {"level":3,"isLink":true,"fontSize":"large"} /-->
 				<!-- wp:post-date {"textAlign":"right","isLink":true} /-->
 			</div>
 			<!-- /wp:group -->

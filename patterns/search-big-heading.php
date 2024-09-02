14c5e4a5322ec41d6203b0db83c8f3af61625fb6
Jennifer Farhat <jennifer.farhat@webdevstudios.com>
2024-09-02 14:04:23 -0700

    fixing linting issues

--- patterns/search-big-heading.php
+++ patterns/search-big-heading.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Title: Search - Big heading and search form
  * Slug: twentytwentyfive/search-big-heading
@@ -15,10 +14,10 @@
 <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
 	<div class="wp-block-group alignwide"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"var(--wp--custom--font-size--huge)"}}} -->
-		<h3 class="wp-block-heading" style="font-size:var(--wp--custom--font-size--huge);"><?php esc_html_e('What are you looking for?', 'twentytwentyfive'); ?></h3>
+		<h3 class="wp-block-heading" style="font-size:var(--wp--custom--font-size--huge);"><?php esc_html_e( 'What are you looking for?', 'twentytwentyfive' ); ?></h3>
 		<!-- /wp:heading -->
 
-		<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"<?php esc_html_e('Type here...', 'twentytwentyfive'); ?>","buttonText":"<?php esc_html_e('Search', 'twentytwentyfive'); ?>"} /-->
+		<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"<?php esc_html_e( 'Type here...', 'twentytwentyfive' ); ?>","buttonText":"<?php esc_html_e( 'Search', 'twentytwentyfive' ); ?>"} /-->
 	</div>
 	<!-- /wp:group -->
 </div>

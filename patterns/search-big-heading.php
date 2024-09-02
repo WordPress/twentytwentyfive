6470527b81c7eea85414bf5ef5593906ea26557d
Jennifer Farhat <jennifer.farhat@webdevstudios.com>
2024-09-02 13:05:51 -0700

    adding huge custom font size to the h3 of the pattern

--- patterns/search-big-heading.php
+++ patterns/search-big-heading.php
@@ -12,8 +12,8 @@
 
 ?>
 <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"level":3} -->
-	<h3 class="wp-block-heading"><?php esc_html_e( 'What are you looking for?', 'twentytwentyfive' ); ?></h3>
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"var(--wp--custom--font-size--huge)"}}} -->
+	<h3 class="wp-block-heading" style="font-size:var(--wp--custom--font-size--huge);"><?php esc_html_e( 'What are you looking for?', 'twentytwentyfive' ); ?></h3>
 	<!-- /wp:heading -->
 
 	<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"<?php esc_html_e( 'Type here...', 'twentytwentyfive' ); ?>","buttonText":"<?php esc_html_e( 'Search', 'twentytwentyfive' ); ?>"} /-->

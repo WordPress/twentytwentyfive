1c49b9c230da285765db89d98633ebe8869d4b8a
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-09-03 18:50:20 +0600

    add service suscriber section header

---
+++ patterns/services-subscriber-only-section.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Services - Subscriber only section
+ * Slug: twentytwentyfive/services-subscriber-only-section
+ * Categories: call-to-action, featured, services,
+ * Description: A subscriber-only section highlighting exclusive services and offerings.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>

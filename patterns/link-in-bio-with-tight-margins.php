aea59f8c36d8260fcf5648af7dea5fb3171f25e4
Beatriz Fialho <beatriz.fialho@automattic.com>
2024-09-03 17:50:03 +0100

    Add: Link in bio with tight margins pattern
    Adds link in bio with tight margins #189

---
+++ patterns/link-in-bio-with-tight-margins.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: Link in bio with tight margins
+ * Slug: twentytwentyfive/link-in-bio-with-tight-margins
+ * Categories: banner, about, featured
+ * Description: A full-width, full-height link in bio section with an image, a paragraph and social links.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:cover {"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","metadata":{"name":"Link in bio with tight margins"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull has-base-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://tt5.local/wp-content/uploads/2024/09/link-in-bio-image-2.jpg","id":2790,"alt":"Black and white photo focusing on a woman and a child from afar.","dimRatio":0,"customOverlayColor":"#818181","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","isDark":false,"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#818181"></span><img class="wp-block-cover__image-background wp-image-2790" alt="Black and white photo focusing on a woman and a child from afar." src="http://tt5.local/wp-content/uploads/2024/09/link-in-bio-image-2.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group" style="min-height:100%"><!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"x-large"} -->
+<p class="has-text-align-left has-x-large-font-size" style="line-height:1.2">I’m Asahachi Kōno, a Japanese&nbsp;photographer, a member of&nbsp;Los Angeles's Japanese Camera Pictorialists of California. Before returning to Japan, I worked as a photo retoucher.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><a href="#">Instagram</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">X</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">TikTok</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->

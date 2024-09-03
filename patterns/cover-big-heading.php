9edd7c3cd3c4ac11cd5dc8ad1b114a9eaf86e63e
Beatriz Fialho <beatriz.fialho@automattic.com>
2024-09-03 16:59:36 +0100

    Adds cover with big heading
    Adds cover with big heading #187

---
+++ patterns/cover-big-heading.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Cover with big heading
+ * Slug: twentytwentyfive/cover-with-big-heading
+ * Categories: banner, about, featured
+ * Description: A cover section with an opinionated design.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:cover {"overlayColor":"accent-1","isUserOverlayColor":true,"isDark":false,"metadata":{"name":"Cover with big heading"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><span aria-hidden="true" class="wp-block-cover__background has-accent-1-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:image {"id":2761,"sizeSlug":"full","linkDestination":"none","align":"wide"} -->
+<figure class="wp-block-image alignwide size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/comingsoon-fw-image.jpg" alt="Photo of a field full of flowers, a blue sky and a tree." class="wp-image-2761"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"><!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"29vw","letterSpacing":"-0.02em","lineHeight":"1"}}} -->
+<p class="has-text-align-left" style="font-size:29vw;letter-spacing:-0.02em;line-height:1"><strong>Stories</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->

26abaecf455f8c89dedcca7466137ff6013aed90
Sam Toohey <hello@samtoohey.xyz>
2024-09-03 16:28:10 +1000

    Further changes pending feedback

--- patterns/stories-podcast-hero.php
+++ patterns/stories-podcast-hero.php
@@ -11,47 +11,55 @@
  */
 
 ?>
-<!-- wp:group {"align":"full",""style":{"spacing":{"margin":{"top":"0","bottom":"0"},"className":"is-style-style-1","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-accent-2-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
-	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"40%"} -->
-		<div class="wp-block-column" style="flex-basis:40%"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
-		<figure class="wp-block-image size-large">
-			<img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/podcast-hero.webp' ) ); ?>" alt="<?php echo esc_attr_x( 'Picture of a person', 'Podcast Hero Image', 'twentytwentyfive' ); ?>"/>
-		</figure>
-		<!-- /wp:image -->
-		</div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"accent-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-accent-2-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"width":"40%"} -->
+		<div class="wp-block-column" style="flex-basis:40%">
+					<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+					<figure class="wp-block-image size-large">
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/podcast-hero.webp" alt="<?php echo esc_attr_x( 'Picture of a person', 'Alt text for hero image', 'twentytwentyfive' ); ?>"/>
+					</figure>
+					<!-- /wp:image -->		</div>
 		<!-- /wp:column -->
+
 		<!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
 		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%">
-			<!-- wp:heading {"level":2,"fontSize":"x-large"} -->
-			<h2 class="wp-block-heading has-x-large-font-size">The Stories® Podcast</h2>
+			<!-- wp:heading {"fontSize":"xx-large"} -->
+			<h2 class="wp-block-heading has-xx-large-font-size"><?php echo esc_html_x( 'The Stories® Podcast', 'Podcast title', 'twentytwentyfive' ); ?></h2>
 			<!-- /wp:heading -->
+
 			<!-- wp:paragraph {"fontSize":"medium"} -->
-			<p class="has-medium-font-size">Storytelling, expert analysis, and vivid descriptions. The Stories® Podcast brings history to life, making it accessible and engaging for a global audience.</p>
+			<p class="has-medium-font-size"><?php echo esc_html_x( 'Storytelling, expert analysis, and vivid descriptions. The Stories® Podcast brings history to life, making it accessible and engaging for a global audience.', 'Podcast description', 'twentytwentyfive' ); ?></p>
 			<!-- /wp:paragraph -->
-			<!-- wp:heading {"level":3} -->
-			<h3 class="wp-block-heading">Subscribe on your favorite platform</h3>
+
+			<!-- wp:heading {"level":3,"fontSize":"large"} -->
+			<h3 class="wp-block-heading has-large-font-size"><?php echo esc_html_x( 'Subscribe on your favorite platform', 'Subscription heading', 'twentytwentyfive' ); ?></h3>
 			<!-- /wp:heading -->
-			<!-- wp:buttons {"fontSize":"x-small","layout":{"type":"flex","justifyContent":"left"}} -->
-				<div class="wp-block-buttons has-custom-font-size has-x-small-font-size"><!-- wp:button {"className":"is-style-outline","fontSize":"extra-small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline has-extra-small-font-size"><a class="wp-block-button__link wp-element-button">Youtube</a></div>
+
+			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|20"}}} -->
+			<div class="wp-block-buttons">
+				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline has-extra-small-font-size"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'Youtube', 'Button text', 'twentytwentyfive' ); ?></a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline has-extra-small-font-size"><a class="wp-block-button__link wp-element-button">Apple Podcasts</a></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'Apple Podcasts', 'Button text', 'twentytwentyfive' ); ?></a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline has-extra-small-font-size"><a class="wp-block-button__link wp-element-button">Spotify</a></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'Spotify', 'Button text', 'twentytwentyfive' ); ?></a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline has-extra-small-font-size"><a class="wp-block-button__link wp-element-button">Pocket Casts</a></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'Pocket Casts', 'Button text', 'twentytwentyfive' ); ?></a></div>
 				<!-- /wp:button -->
 
-				<!-- wp:button {"className":"is-style-outline","fontSize":"extra-small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-outline has-extra-small-font-size"><a class="wp-block-button__link wp-element-button">RSS</a></div>
-				<!-- /wp:button --></div>
+				<!-- wp:button {"className":"is-style-outline","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( 'RSS', 'Button text', 'twentytwentyfive' ); ?></a></div>
+				<!-- /wp:button -->
+			</div>
 			<!-- /wp:buttons -->
 		</div>
 		<!-- /wp:column -->

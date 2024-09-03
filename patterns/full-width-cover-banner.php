9bcdec6fe29be511bfe2de6a43efd27831baeb8f
Beatriz Fialho <beatriz.fialho@automattic.com>
2024-09-03 12:35:16 +0100

    Add: Full-width cover banner pattern

---
+++ patterns/full-width-cover-banner.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Full-width Cover Banner
+ * Slug: twentytwentyfive/full-width-cover-banner
+ * Categories: banner, call-to-action, media, featured
+ * Description: A full-width cover banner that can be applied to a page or it can work as a single landing page.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:cover {"url":"http://tt5.local/wp-content/uploads/2024/09/comingsoon-fw-image.jpg","id":2761,"alt":"Photo of a field full of flowers, a blue sky and a tree.","dimRatio":30,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","metadata":{"name":"Full-width cover banner"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-1"}},"heading":{"color":{"text":"var:preset|color|accent-1"}}},"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"textColor":"accent-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-accent-1-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-30 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2761" alt="Photo of a field full of flowers, a blue sky and a tree." src="http://tt5.local/wp-content/uploads/2024/09/comingsoon-fw-image.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"small"} -->
+<h1 class="wp-block-heading has-text-align-center has-small-font-size">Event</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-xx-large-font-size">Something great is coming soon</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Subscribe to get notified when our website is ready.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"accent-1","textColor":"contrast","className":"is-style-fill","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}}} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-contrast-color has-accent-1-background-color has-text-color has-background has-link-color wp-element-button">Subscribe</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->

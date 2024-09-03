b3bec52c9a9ae6e1211cec787433729ef407d757
Beatriz Fialho <beatriz.fialho@automattic.com>
2024-09-03 17:16:32 +0100

    Add: Link in bio heading, paragraph, links and full-height image pattern
    Adds link in bio heading, paragraph, links and full-height image pattern #188
    
    I applied one of the existing color presets, instead of the green color from the Figma.

---
+++ patterns/link-in-bio-heading-paragraph-links-image.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Title: Link in bio heading, paragraph, links and full-height image
+ * Slug: twentytwentyfive/link-in-bio-heading-paragraph-links-image
+ * Categories: banner, about, featured
+ * Description: A link in bio landing page with a heading, paragraph, links and a full height image.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:cover {"overlayColor":"accent-3","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","metadata":{"name":"Link in bio heading, paragraph, links and full-height image"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}},"heading":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"blockGap":"0","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"textColor":"secondary","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-secondary-color has-text-color has-link-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-accent-3-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":null,"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading -->
+<h2 class="wp-block-heading">Lewis Hine</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lewis W. Hine studied sociology before moving to New York in 1901 to work at the Ethical Culture School, where he took up photography to enhance his teaching practices</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><a href="#">Instagram</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">X</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">TikTok</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://tt5.local/wp-content/uploads/2024/09/linkinbio-image.jpg","id":2791,"alt":"Photo of a woman worker.","dimRatio":0,"customOverlayColor":"#6b6b6b","minHeight":100,"minHeightUnit":"vh","layout":{"type":"default"}} -->
+<div class="wp-block-cover" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#6b6b6b"></span><img class="wp-block-cover__image-background wp-image-2791" alt="Photo of a woman worker." src="http://tt5.local/wp-content/uploads/2024/09/linkinbio-image.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->

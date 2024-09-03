8de28b43fc9aca884843652528922c1dcf6bd244
davidbp845 <davidbp845@riseup.net>
2024-09-03 09:02:13 +0200

    added events three columns pattern with rawpixel image

---
+++ patterns/events-three-columns-images-and-titles.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Title: Events three columns with event images and titles
+ * Slug: twentytwentyfive/events-three-columns-images-and-titles
+ * Categories: featured, query
+ * Description: A header with title and text and three columns that show 3 events with their images and titles.
+ * Keywords: events, columns, images
+ * 
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+?>
+<!-- wp:group {"metadata":{"categories":["featured"],"patternName":"twentytwentyfive/events-three-columns-images-and-titles","name":"Events three columns with event images and titles"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading -->
+<h2 class="wp-block-heading">Events</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>These are some of the upcoming events.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|50"},"padding":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-top:0;padding-bottom:0"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70"},"blockGap":"0"}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--70)"><!-- wp:image {"id":89,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-8802835-jpeg-scaled.webp" alt="" class="wp-image-89"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-primary-color has-text-color has-link-color has-medium-font-size" style="padding-top:var(--wp--preset--spacing--40)">September 2, 2024</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":3,"style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}}}} -->
+<h3 class="wp-block-heading" style="padding-top:var(--wp--preset--spacing--20)">Tell your story</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"fontSize":"large"} -->
+<p class="has-large-font-size" style="padding-top:var(--wp--preset--spacing--40)"><a href="#a">Event details</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70"},"blockGap":"0"}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--70)"><!-- wp:image {"id":89,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-8802835-jpeg-scaled.webp" alt="" class="wp-image-89"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-primary-color has-text-color has-link-color has-medium-font-size" style="padding-top:var(--wp--preset--spacing--40)">September 2, 2024</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":3,"style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}}}} -->
+<h3 class="wp-block-heading" style="padding-top:var(--wp--preset--spacing--20)">Tell your story</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"fontSize":"large"} -->
+<p class="has-large-font-size" style="padding-top:var(--wp--preset--spacing--40)"><a href="#a">Event details</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70"},"blockGap":"0"}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--70)"><!-- wp:image {"id":89,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-8802835-jpeg-scaled.webp" alt="" class="wp-image-89"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-primary-color has-text-color has-link-color has-medium-font-size" style="padding-top:var(--wp--preset--spacing--40)">September 2, 2024</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":3,"style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}}}} -->
+<h3 class="wp-block-heading" style="padding-top:var(--wp--preset--spacing--20)">Tell your story</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"fontSize":"large"} -->
+<p class="has-large-font-size" style="padding-top:var(--wp--preset--spacing--40)"><a href="#a">Event details</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->

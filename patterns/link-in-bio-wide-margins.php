80ae2d4cef8f25091c66763730b05d3ef6f1bbb9
Juan Aldasoro <juanfraa@gmail.com>
2024-09-03 18:49:06 +0200

    Adjust pattern code.

--- patterns/link-in-bio-wide-margins.php
+++ patterns/link-in-bio-wide-margins.php
@@ -11,32 +11,42 @@
  */
 
 ?>
-<!-- wp:cover {"overlayColor":"secondary","isUserOverlayColor":true,"isDark":false,"metadata":{"name":"Link in bio with profile, links and wide margins"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|80","left":"var:preset|spacing|80","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":2711,"scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","style":{"border":{"radius":{"topLeft":"150px","bottomRight":"150px"}}}} -->
-<figure class="wp-block-image aligncenter size-full has-custom-border"><img src="http://tt5.local/wp-content/uploads/2024/09/cvbio_image-1.webp" alt="Portrait of a woman." class="wp-image-2711" style="border-top-left-radius:150px;border-bottom-right-radius:150px;object-fit:cover"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
+<!-- wp:cover {"overlayColor":"secondary","isUserOverlayColor":true,"isDark":false,"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|80","left":"var:preset|spacing|80","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-100 has-background-dim"></span>
+<div class="wp-block-cover__inner-container">
+	<!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+	<div class="wp-block-columns alignwide are-vertically-aligned-center">
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+			<!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","style":{"border":{"radius":{"topLeft":"150px","bottomRight":"150px"}}}} -->
+			<figure class="wp-block-image aligncenter size-full has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cv-bio.webp" alt="Portrait of a woman." style="border-top-left-radius:150px;border-bottom-right-radius:150px;object-fit:cover"/></figure>
+			<!-- /wp:image --></div>
+		<!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"left"} -->
-<h2 class="wp-block-heading has-text-align-left">Gail Falk Seltzer</h2>
-<!-- /wp:heading -->
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"textAlign":"left"} -->
+				<h2 class="wp-block-heading has-text-align-left">Gail Falk Seltzer</h2>
+				<!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>I’m Gail, a passionate and Staff Lawyer. Based in Charleston. I’m a graduate of Yale University.</p>
-<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>I’m Gail, a passionate and Staff Lawyer. Based in Charleston. I’m a graduate of Yale University.</p>
+				<!-- /wp:paragraph -->
 
-<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#111111","className":"is-style-logos-only"} -->
-<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"x"} /-->
+				<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#111111","className":"is-style-logos-only"} -->
+				<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"x"} /-->
 
-<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+				<!-- wp:social-link {"url":"#","service":"instagram"} /-->
 
-<!-- wp:social-link {"url":"#","service":"whatsapp"} /--></ul>
-<!-- /wp:social-links --></div>
-<!-- /wp:group --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
+				<!-- wp:social-link {"url":"#","service":"whatsapp"} /--></ul>
+				<!-- /wp:social-links -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div></div>
 <!-- /wp:cover -->

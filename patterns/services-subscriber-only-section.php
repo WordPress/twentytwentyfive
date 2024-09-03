83184aab9e7c6f8f68657cdcc959ab10daf69430
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-09-03 18:52:20 +0600

    add service subscribed only section patterns

--- patterns/services-subscriber-only-section.php
+++ patterns/services-subscriber-only-section.php
@@ -11,3 +11,70 @@
  */
 
 ?>
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"nowrap"}} -->
+<div class="wp-block-buttons alignwide"><!-- wp:button {"className":"is-style-outline","style":{"border":{"style":"solid","width":"1px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small","borderColor":"contrast"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size" style="font-style:normal;font-weight:400"><a class="wp-block-button__link has-border-color has-contrast-border-color wp-element-button" style="border-style:solid;border-width:1px">Become a member</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Subscribe to get unlimited access</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">get access to our paid articles and weekly newsletter.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">Join our IRL event.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">Get a free tote beg</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">an elegant addition home decor collection.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">Join our forums.</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"right"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"27%","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:27%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Subscribe</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:buttons {"layout":{"type":"flex"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">View plans</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">cancel or pause any time.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":76,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/c/wp-content/uploads/2024/08/screens.webp" alt="" class="wp-image-76"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->

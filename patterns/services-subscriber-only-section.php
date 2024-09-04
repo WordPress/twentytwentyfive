ee50073a16a4aec3178fac4489b82d6c564af0c8
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-09-04 08:45:05 +0600

    update button style to pill with bg color gray and border radius 4px

--- patterns/services-subscriber-only-section.php
+++ patterns/services-subscriber-only-section.php
@@ -11,12 +11,12 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull"><!-- wp:columns {"align":"wide"} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"nowrap"}} -->
-<div class="wp-block-buttons alignwide"><!-- wp:button {"className":"is-style-outline","style":{"border":{"style":"solid","width":"1px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small","borderColor":"contrast"} -->
-<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size" style="font-style:normal;font-weight:400"><a class="wp-block-button__link has-border-color has-contrast-border-color wp-element-button" style="border-style:solid;border-width:1px">Become a member</a></div>
+<div class="wp-block-buttons alignwide"><!-- wp:button {"textColor":"base","className":"is-style-outline","style":{"border":{"style":"none","width":"0px","radius":"4px"},"typography":{"fontStyle":"normal","fontWeight":"400"},"color":{"background":"#808080"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"fontSize":"small"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size" style="font-style:normal;font-weight:400"><a class="wp-block-button__link has-base-color has-text-color has-background has-link-color wp-element-button" style="border-style:none;border-width:0px;border-radius:4px;background-color:#808080">Become a member</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
 
@@ -50,16 +50,16 @@
 <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"27%","layout":{"type":"default"}} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:27%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Subscribe</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Subscribe</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:buttons {"layout":{"type":"flex"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">View plans</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link wp-element-button">View plans</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:column --></div>
@@ -73,7 +73,7 @@
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":76,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services-subscriber-photo.webp" alt="Smartphones capturing a scenic wildflower meadow with trees" class="wp-image-76"/></figure>
+<figure class="wp-block-image size-full"><img src="http://localhost/d/wp-content/themes/twentytwentyfive/assets/images/services-subscriber-photo.webp" alt="Smartphones capturing a scenic wildflower meadow with trees" class="wp-image-76"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>

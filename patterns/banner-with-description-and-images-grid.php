cd01ac0b72230782a19f695ac0174ae4ef2ac75f
Juan Aldasoro <juanfraa@gmail.com>
2024-09-04 18:43:16 +0200

    Update pattern code.

--- patterns/banner-with-description-and-images-grid.php
+++ patterns/banner-with-description-and-images-grid.php
@@ -11,26 +11,34 @@
  */
 
 ?>
-<!-- wp:group {"metadata":{"name":"Banner with description and images grid"},"align":"full","className":"is-style-default","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|60"}},"layout":{"type":"grid","minimumColumnWidth":"32rem"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
-<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group"><!-- wp:heading {"fontSize":"small"} -->
-<h2 class="wp-block-heading has-small-font-size">About Us</h2>
-<!-- /wp:heading -->
+<!-- wp:group {"align":"full","className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default">
+	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|60"}},"layout":{"type":"grid","minimumColumnWidth":"32rem"}} -->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"fontSize":"small"} -->
+				<h2 class="wp-block-heading has-small-font-size">About Us</h2>
+				<!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><strong>Fleurs</strong> is a flower delivery and subscription business. Based in the EU, our mission is not only to deliver stunning flower arrangements across but also foster knowledge and enthusiasm on the beautiful gift of nature: flowers.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group -->
+				<!-- wp:paragraph -->
+				<p><strong>Fleurs</strong> is a flower delivery and subscription business. Based in the EU, our mission is not only to deliver stunning flower arrangements across but also foster knowledge and enthusiasm on the beautiful gift of nature: flowers.</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
 
-<!-- wp:image {"id":2753,"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/grid-flower-1.jpg" alt="Photography close up of a red flower." class="wp-image-2753" style="aspect-ratio:16/9;object-fit:cover"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:group -->
+			<!-- wp:image {"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/grid-flower-1.webp" alt="Photography close up of a red flower." style="aspect-ratio:16/9;object-fit:cover"/></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:group -->
 
-<!-- wp:image {"id":2754,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/grid-flower-2.jpg" alt="Black and white photography close up of a flower." class="wp-image-2754" style="aspect-ratio:1;object-fit:cover"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:group --></div>
+		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/grid-flower-2.webp" alt="Black and white photography close up of a flower." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:group -->
+</div>
 <!-- /wp:group -->

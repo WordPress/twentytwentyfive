e3e8eb3ac185ed2f0eed6b045305681414a3b88c
Beatriz Fialho <beatriz.fialho@automattic.com>
2024-09-05 11:38:59 +0100

    Add: Instagram grid pattern
    Adds instagram grid pattern #222

---
+++ patterns/instagram-grid.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Instagram grid
+ * Slug: twentytwentyfive/instagram-grid
+ * Categories: media, featured
+ * Description: A grid section with photos and a link to an Instagram profile.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Instagram grid"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","minimumColumnWidth":"18rem"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"className":"is-style-section-3","layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-3"><!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+<div class="wp-block-group" style="min-height:100%"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Instagram</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">@<a href="#">example</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"id":2910,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image1.webp" alt="Photo of a field full of flowers, a blue sky and a tree." class="wp-image-2910" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":2911,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image2.webp" alt="Profile portrait of a native person." class="wp-image-2911" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":2912,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image3.webp" alt="View of the deep ocean." class="wp-image-2912" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":2913,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image4.webp" alt="A native New Zealander on a small boat at the beach." class="wp-image-2913" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":2914,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image5.webp" alt="The acropolis in Athens." class="wp-image-2914" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":2915,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image6.webp" alt="Close up of two flowers on a dark background." class="wp-image-2915" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":2916,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://tt5.local/wp-content/uploads/2024/09/ig-grid-image7.webp" alt="Birds on a lake." class="wp-image-2916" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

be35035414a1fb2575f88f8a2f833e6f84a628cd
Carolina Nymark <hi@themesbycarolina.com>
2024-09-05 11:35:34 +0200

    Update patterns/event-rsvp.php

--- patterns/event-rsvp.php
+++ patterns/event-rsvp.php
@@ -4,6 +4,7 @@
  * Slug: twentytwentyfive/event-rsvp
  * Keywords: call-to-action, rsvp, event
  * Categories: call-to-action
+ * Post Types: page, wp_template
  *
  * @package WordPress
  * @subpackage Twenty_Twenty_Five

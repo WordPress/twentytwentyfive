adad9d21e46591df13f6ddd31eece3aba133686a
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-09-05 08:11:23 +0600

    fix spelling, responsive, and spacing issues

--- patterns/services-subscriber-only-section.php
+++ patterns/services-subscriber-only-section.php
@@ -11,22 +11,20 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide"} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|70","left":"var:preset|spacing|70"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"nowrap"}} -->
-<div class="wp-block-buttons alignwide"><!-- wp:button {"textColor":"base","className":"is-style-outline","style":{"border":{"style":"none","width":"0px","radius":"4px"},"typography":{"fontStyle":"normal","fontWeight":"400"},"color":{"background":"#808080"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"fontSize":"small"} -->
-<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size" style="font-style:normal;font-weight:400"><a class="wp-block-button__link has-base-color has-text-color has-background has-link-color wp-element-button" style="border-style:none;border-width:0px;border-radius:4px;background-color:#808080">Become a member</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons -->
-
-<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
-<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Subscribe to get unlimited access</h2>
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+<h2 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:700">Become a member</h2>
 <!-- /wp:heading -->
 
-<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item {"fontSize":"medium"} -->
-<li class="has-medium-font-size">get access to our paid articles and weekly newsletter.</li>
+<!-- wp:heading {"fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-xx-large-font-size">Subscribe to get unlimited access</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
+<ul style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)" class="wp-block-list"><!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">Get access to our paid articles and weekly newsletter.</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"fontSize":"medium"} -->
@@ -34,11 +32,11 @@
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"fontSize":"medium"} -->
-<li class="has-medium-font-size">Get a free tote beg</li>
+<li class="has-medium-font-size">Get a free tote bag.</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"fontSize":"medium"} -->
-<li class="has-medium-font-size">an elegant addition home decor collection.</li>
+<li class="has-medium-font-size">An elegant addition of home decor collection.</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"fontSize":"medium"} -->
@@ -46,28 +44,18 @@
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"right"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"27%","layout":{"type":"default"}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:27%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"width":100} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Subscribe</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column -->
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left","flexWrap":"nowrap"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Subscribe</a></div>
+<!-- /wp:button -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:buttons {"layout":{"type":"flex"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-outline"} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link wp-element-button">View plans</a></div>
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">View plans</a></div>
 <!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->
+<!-- /wp:buttons -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size">cancel or pause any time.</p>
+<p class="has-small-font-size">Cancel or pause anytime.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 

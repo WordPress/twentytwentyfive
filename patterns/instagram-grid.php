1bcdc127fb0014623a98e5108c362c4d5bb583d2
Juan Aldasoro <juanfraa@gmail.com>
2024-09-06 18:24:35 +0200

    Update pattern to use cover. Update image filenames.

--- patterns/instagram-grid.php
+++ patterns/instagram-grid.php
@@ -15,48 +15,50 @@
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
 	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","minimumColumnWidth":"18rem"}} -->
 	<div class="wp-block-group alignwide">
-		<!-- wp:group {"className":"is-style-section-3","layout":{"type":"default"}} -->
-		<div class="wp-block-group is-style-section-3">
-			<!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-			<div class="wp-block-group" style="min-height:100%">
-				<!-- wp:heading {"fontSize":"large"} -->
-				<h2 class="wp-block-heading has-large-font-size">Instagram</h2>
-				<!-- /wp:heading -->
+		<!-- wp:cover {"overlayColor":"accent-1","isUserOverlayColor":true,"isDark":false,"style":{"dimensions":{"aspectRatio":"1"}}} -->
+		<div class="wp-block-cover is-light">
+			<span aria-hidden="true" class="wp-block-cover__background has-accent-1-background-color has-background-dim-100 has-background-dim"></span>
+			<div class="wp-block-cover__inner-container">
+				<!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+				<div class="wp-block-group" style="min-height:100%"><!-- wp:heading {"fontSize":"large"} -->
+					<h2 class="wp-block-heading has-large-font-size">Instagram</h2>
+					<!-- /wp:heading -->
 
-				<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-				<p class="has-text-align-center has-medium-font-size">@<a href="#">example</a></p>
-				<!-- /wp:paragraph -->
+					<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+					<p class="has-text-align-center has-medium-font-size">@<a href="#">example</a></p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
 			</div>
-			<!-- /wp:group -->
 		</div>
-		<!-- /wp:group -->
+		<!-- /wp:cover -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-1.webp" alt="Photo of a field full of flowers, a blue sky and a tree." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hibiscus-flower-square.webp" alt="Photo of a field full of flowers, a blue sky and a tree." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-2.webp" alt="Profile portrait of a native person." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/vash-gon-square.webp" alt="Profile portrait of a native person." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-3.webp" alt="View of the deep ocean." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/coral-square.webp" alt="View of the deep ocean." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-4.webp" alt="A native New Zealander on a small boat at the beach." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/fair-sauimatani-square.webp" alt="A native New Zealander on a small boat at the beach." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-5.webp" alt="The acropolis in Athens." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/parthenon-square.webp" alt="The acropolis in Athens." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-6.webp" alt="Close up of two flowers on a dark background." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/dallas-creek-square.webp" alt="Close up of two flowers on a dark background." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 
 		<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ig-grid-image-7.webp" alt="Birds on a lake." style="aspect-ratio:1;object-fit:cover"/></figure>
+		<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marshland-birds-square.webp" alt="Birds on a lake." style="aspect-ratio:1;object-fit:cover"/></figure>
 		<!-- /wp:image -->
 	</div>
 	<!-- /wp:group -->

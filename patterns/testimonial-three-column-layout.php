545c1bdc328bbae7b120164326005a56d983f652
Faisal Ahammad <faisalahammad24@gmail.com>
2024-09-08 00:28:59 +0600

    Testimonial - Three column layout with 6 testimonials

---
+++ patterns/testimonial-three-column-layout.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Title: Testimonial Three column layout
+ * Slug: twentytwentyfive/testimonial-three-column-layout
+ * Categories: testimonial, column, review
+ * Description: A three column layout with 6 testimonials.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ *
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"50px","bottom":"50px","left":"50px","right":"50px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:50px;padding-right:50px;padding-bottom:50px;padding-left:50px"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontSize":"70px","lineHeight":"1.1","letterSpacing":"-2%","fontStyle":"normal","fontWeight":"500"}},"fontFamily":"manrope"} -->
+<h2 class="wp-block-heading has-manrope-font-family" style="font-size:70px;font-style:normal;font-weight:500;letter-spacing:-2%;line-height:1.1">What people are saying</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:30px;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0px","left":"50px"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"opacity-20"} -->
+<div class="wp-block-column has-border-color has-opacity-20-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"38px","lineHeight":"1.1","letterSpacing":"-2%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:38px;letter-spacing:-2%;line-height:1.1">“Amazing quality and care. I love all your products.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","lineHeight":"1.1","letterSpacing":"-1%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:22px;letter-spacing:-1%;line-height:1.1">Jo Mulligan</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","lineHeight":"1.3","letterSpacing":"-1%"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="margin-top:var(--wp--preset--spacing--20);font-size:12px;letter-spacing:-1%;line-height:1.3">Atlanta, GA</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"opacity-20"} -->
+<div class="wp-block-column has-border-color has-opacity-20-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"38px","lineHeight":"1.1","letterSpacing":"-2%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:38px;letter-spacing:-2%;line-height:1.1">“Amazing quality and care. I love all your products.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","lineHeight":"1.1","letterSpacing":"-1%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:22px;letter-spacing:-1%;line-height:1.1">Otto Reid</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","lineHeight":"1.3","letterSpacing":"-1%"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="margin-top:var(--wp--preset--spacing--20);font-size:12px;letter-spacing:-1%;line-height:1.3">Springfield, IL</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"opacity-20"} -->
+<div class="wp-block-column has-border-color has-opacity-20-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"38px","lineHeight":"1.1","letterSpacing":"-2%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:38px;letter-spacing:-2%;line-height:1.1">“Amazing quality and care. I love all your products.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","lineHeight":"1.1","letterSpacing":"-1%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:22px;letter-spacing:-1%;line-height:1.1">Jo Mulligan</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","lineHeight":"1.3","letterSpacing":"-1%"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="margin-top:var(--wp--preset--spacing--20);font-size:12px;letter-spacing:-1%;line-height:1.3">Atlanta, GA</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0px","left":"50px"},"margin":{"top":"50px"}}}} -->
+<div class="wp-block-columns" style="margin-top:50px"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"opacity-20"} -->
+<div class="wp-block-column has-border-color has-opacity-20-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"38px","lineHeight":"1.1","letterSpacing":"-2%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:38px;letter-spacing:-2%;line-height:1.1">“Amazing quality and care. I love all your products.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","lineHeight":"1.1","letterSpacing":"-1%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:22px;letter-spacing:-1%;line-height:1.1">Jo Mulligan</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","lineHeight":"1.3","letterSpacing":"-1%"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="margin-top:var(--wp--preset--spacing--20);font-size:12px;letter-spacing:-1%;line-height:1.3">Atlanta, GA</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"opacity-20"} -->
+<div class="wp-block-column has-border-color has-opacity-20-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"38px","lineHeight":"1.1","letterSpacing":"-2%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:38px;letter-spacing:-2%;line-height:1.1">“Amazing quality and care. I love all your products.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","lineHeight":"1.1","letterSpacing":"-1%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:22px;letter-spacing:-1%;line-height:1.1">Otto Reid</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","lineHeight":"1.3","letterSpacing":"-1%"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="margin-top:var(--wp--preset--spacing--20);font-size:12px;letter-spacing:-1%;line-height:1.3">Springfield, IL</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"opacity-20"} -->
+<div class="wp-block-column has-border-color has-opacity-20-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"38px","lineHeight":"1.1","letterSpacing":"-2%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:38px;letter-spacing:-2%;line-height:1.1">“Amazing quality and care. I love all your products.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px","lineHeight":"1.1","letterSpacing":"-1%"}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="font-size:22px;letter-spacing:-1%;line-height:1.1">Jo Mulligan</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","lineHeight":"1.3","letterSpacing":"-1%"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontFamily":"manrope"} -->
+<p class="has-manrope-font-family" style="margin-top:var(--wp--preset--spacing--20);font-size:12px;letter-spacing:-1%;line-height:1.3">Atlanta, GA</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

9338b0e7757c171fc45b1c0eda4a1f9b64461587
gohelkunjan <gohel.kunjan@gmail.com>
2024-09-09 00:22:03 +0530

    Events - Event schedule pattern design

--- patterns/events-event-schedule.php
+++ patterns/events-event-schedule.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Title: Events - Event schedule
  * Slug: twentytwentyfive/events-event-schedule
@@ -14,173 +13,173 @@
 ?>
 <!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide">
-    <!-- wp:group {"layout":{"type":"default"}} -->
-    <div class="wp-block-group">
-        <!-- wp:heading {"style":{"typography":{"fontSize":"42px"}},"fontFamily":"manrope"} -->
-        <h2 class="wp-block-heading has-manrope-font-family" style="font-size:42px">Agenda</h2>
-        <!-- /wp:heading -->
+	<!-- wp:group {"layout":{"type":"default"}} -->
+	<div class="wp-block-group">
+		<!-- wp:heading {"style":{"typography":{"fontSize":"42px"}},"fontFamily":"manrope"} -->
+		<h2 class="wp-block-heading has-manrope-font-family" style="font-size:42px">Agenda</h2>
+		<!-- /wp:heading -->
 
-        <!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
-        <p class="has-manrope-font-family" style="font-size:22px">These are some of the upcoming events.</p>
-        <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+		<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
+		<p class="has-manrope-font-family" style="font-size:22px">These are some of the upcoming events.</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
 
-    <!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"30px","bottom":"30px"}}}} -->
-    <hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:30px;margin-bottom:30px" />
-    <!-- /wp:separator -->
+	<!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"30px","bottom":"30px"}}}} -->
+	<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:30px;margin-bottom:30px" />
+	<!-- /wp:separator -->
 
-    <!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-        <div class="wp-block-group alignwide"><!-- wp:columns {"style":{"spacing":{"margin":{"top":"50px"}}}} -->
-            <div class="wp-block-columns" style="margin-top:50px"><!-- wp:column {"width":"36%"} -->
-                <div class="wp-block-column" style="flex-basis:36%"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"26px"}},"fontFamily":"manrope"} -->
-                    <h3 class="wp-block-heading has-manrope-font-family" style="font-size:26px">Friday, Feb. 1</h3>
-                <!-- /wp:heading --></div>
-            <!-- /wp:column -->
+	<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+		<div class="wp-block-group alignwide"><!-- wp:columns {"style":{"spacing":{"margin":{"top":"50px"}}}} -->
+			<div class="wp-block-columns" style="margin-top:50px"><!-- wp:column {"width":"36%"} -->
+				<div class="wp-block-column" style="flex-basis:36%"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"26px"}},"fontFamily":"manrope"} -->
+					<h3 class="wp-block-heading has-manrope-font-family" style="font-size:26px">Friday, Feb. 1</h3>
+				<!-- /wp:heading --></div>
+			<!-- /wp:column -->
 
-            <!-- wp:column -->
-            <div class="wp-block-column">
-                <!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"50px"}}}} -->
-                <div class="wp-block-columns" style="margin-bottom:50px">
-                    <!-- wp:column {"width":"26.5%"} -->
-                    <div class="wp-block-column" style="flex-basis:26.5%">
-                        <!-- wp:image {"width":"180px","sizeSlug":"full","linkDestination":"none"} -->
-                        <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/agenda-img-1.webp"
-                                alt="" style="width:180px" /></figure>
-                        <!-- /wp:image -->
-                    </div>
-                    <!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="wp-block-column">
+				<!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"50px"}}}} -->
+				<div class="wp-block-columns" style="margin-bottom:50px">
+					<!-- wp:column {"width":"26.5%"} -->
+					<div class="wp-block-column" style="flex-basis:26.5%">
+						<!-- wp:image {"width":"180px","sizeSlug":"full","linkDestination":"none"} -->
+						<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/agenda-img-1.webp"
+								alt="" style="width:180px" /></figure>
+						<!-- /wp:image -->
+					</div>
+					<!-- /wp:column -->
 
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                        <!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
-                        <h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">Fauna from North America and its characteristics</h4>
-                        <!-- /wp:heading -->
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
+						<h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">Fauna from North America and its characteristics</h4>
+						<!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
-                        <!-- /wp:paragraph -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
+						<!-- /wp:paragraph -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+				</div>
+				<!-- /wp:columns -->
 
-                <!-- wp:columns -->
-                <div class="wp-block-columns">
-                    <!-- wp:column {"width":"26.5%"} -->
-                    <div class="wp-block-column" style="flex-basis:26.5%">
-                        <!-- wp:image {"width":"180px","height":"180px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-                        <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/agenda-img-2.webp"
-                                alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
-                        <!-- /wp:image -->
-                    </div>
-                    <!-- /wp:column -->
+				<!-- wp:columns -->
+				<div class="wp-block-columns">
+					<!-- wp:column {"width":"26.5%"} -->
+					<div class="wp-block-column" style="flex-basis:26.5%">
+						<!-- wp:image {"width":"180px","height":"180px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+						<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/agenda-img-2.webp"
+								alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
+						<!-- /wp:image -->
+					</div>
+					<!-- /wp:column -->
 
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                        <!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
-                        <h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">Things you didn’t know about the deep ocean</h4>
-                        <!-- /wp:heading -->
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
+						<h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">Things you didn’t know about the deep ocean</h4>
+						<!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
-                        <!-- /wp:paragraph -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
+						<!-- /wp:paragraph -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-            </div>
-            <!-- /wp:column -->
-        </div>
-        <!-- /wp:columns -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+				</div>
+				<!-- /wp:columns -->
+			</div>
+			<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
 
-        <!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"50px","bottom":"50px"}}}} -->
-        <hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"
-            style="margin-top:50px;margin-bottom:50px" />
-        <!-- /wp:separator -->
+		<!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"50px","bottom":"50px"}}}} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"
+			style="margin-top:50px;margin-bottom:50px" />
+		<!-- /wp:separator -->
 
-        <!-- wp:columns -->
-        <div class="wp-block-columns">
-            <!-- wp:column {"width":"36%"} -->
-            <div class="wp-block-column" style="flex-basis:36%">
-                <!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"26px"}},"fontFamily":"manrope"} -->
-                <h3 class="wp-block-heading has-manrope-font-family" style="font-size:26px">Saturday, Feb. 2</h3>
-                <!-- /wp:heading -->
-            </div>
-            <!-- /wp:column -->
+		<!-- wp:columns -->
+		<div class="wp-block-columns">
+			<!-- wp:column {"width":"36%"} -->
+			<div class="wp-block-column" style="flex-basis:36%">
+				<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"26px"}},"fontFamily":"manrope"} -->
+				<h3 class="wp-block-heading has-manrope-font-family" style="font-size:26px">Saturday, Feb. 2</h3>
+				<!-- /wp:heading -->
+			</div>
+			<!-- /wp:column -->
 
-            <!-- wp:column -->
-            <div class="wp-block-column">
-                <!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"50px"}}}} -->
-                <div class="wp-block-columns" style="margin-bottom:50px">
-                    <!-- wp:column {"width":"26.5%"} -->
-                    <div class="wp-block-column" style="flex-basis:26.5%">
-                        <!-- wp:image {"width":"180px","height":"180px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-                        <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/agenda-img-3.webp"
-                                alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
-                        <!-- /wp:image -->
-                    </div>
-                    <!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="wp-block-column">
+				<!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"50px"}}}} -->
+				<div class="wp-block-columns" style="margin-bottom:50px">
+					<!-- wp:column {"width":"26.5%"} -->
+					<div class="wp-block-column" style="flex-basis:26.5%">
+						<!-- wp:image {"width":"180px","height":"180px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+						<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/agenda-img-3.webp"
+								alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
+						<!-- /wp:image -->
+					</div>
+					<!-- /wp:column -->
 
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                        <!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
-                        <h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">Things you didn’t know about the deep ocean</h4>
-                        <!-- /wp:heading -->
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
+						<h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">Things you didn’t know about the deep ocean</h4>
+						<!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
-                        <!-- /wp:paragraph -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
+						<!-- /wp:paragraph -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+				</div>
+				<!-- /wp:columns -->
 
-                <!-- wp:columns -->
-                <div class="wp-block-columns">
-                    <!-- wp:column {"width":"26.5%"} -->
-                    <div class="wp-block-column" style="flex-basis:26.5%">
-                        <!-- wp:image {"width":"180px","height":"180px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-                        <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/agenda-img-4.webp"
-                                alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
-                        <!-- /wp:image -->
-                    </div>
-                    <!-- /wp:column -->
+				<!-- wp:columns -->
+				<div class="wp-block-columns">
+					<!-- wp:column {"width":"26.5%"} -->
+					<div class="wp-block-column" style="flex-basis:26.5%">
+						<!-- wp:image {"width":"180px","height":"180px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+						<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/agenda-img-4.webp"
+								alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
+						<!-- /wp:image -->
+					</div>
+					<!-- /wp:column -->
 
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                        <!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
-                        <h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">An introduction to African dialects</h4>
-                        <!-- /wp:heading -->
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"24px"}},"fontFamily":"manrope"} -->
+						<h4 class="wp-block-heading has-manrope-font-family" style="font-size:24px">An introduction to African dialects</h4>
+						<!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
-                        <!-- /wp:paragraph -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:22px">9 AM — 11 AM</p>
+						<!-- /wp:paragraph -->
 
-                        <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
-                        <p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-            </div>
-            <!-- /wp:column -->
-        </div>
-        <!-- /wp:columns -->
-    </div>
-    <!-- /wp:group -->
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"fontFamily":"manrope"} -->
+						<p class="has-manrope-font-family" style="font-size:16px">Lecture by <a href="#">Prof. Fiona Presley</a></p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+				</div>
+				<!-- /wp:columns -->
+			</div>
+			<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
+	</div>
+	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->

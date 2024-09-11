2eb1ac0284810b22f30de353512aa7c5b2db45ec
Tanvirul Haque <tanvirulhaque123@gmail.com>
2024-09-11 14:32:20 +0600

    Remove - placeholder text from the paragraph

--- patterns/text-faqs.php
+++ patterns/text-faqs.php
@@ -26,7 +26,7 @@
 			<!-- wp:details {"style":{"border":{"bottom":{"color":"var:preset|color|opacity-20","width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
 			<details class="wp-block-details" style="border-bottom-color:var(--wp--preset--color--opacity-20);border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--30);font-style:normal;font-weight:400">
 				<summary><?php echo esc_html_x( 'What is The Stories Book about?', 'Question in the FAQs pattern', 'twentytwentyfive' ); ?></summary>
-				<!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+				<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
 				<p style="font-style:normal;font-weight:300"><?php echo esc_html_x( 'This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein\'s evocative imagery, Strand\'s groundbreaking modernist approach, and Kōno\'s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography.', 'Answer in the FAQs pattern', 'twentytwentyfive' ); ?></p>
 				<!-- /wp:paragraph -->
 			</details>
@@ -35,7 +35,7 @@
 			<!-- wp:details {"style":{"border":{"bottom":{"color":"var:preset|color|opacity-20","width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
 			<details class="wp-block-details" style="border-bottom-color:var(--wp--preset--color--opacity-20);border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--30);font-style:normal;font-weight:400">
 				<summary><?php echo esc_html_x( 'How much does The Stories Book cost?', 'Question in the FAQs pattern', 'twentytwentyfive' ); ?></summary>
-				<!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+				<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
 				<p style="font-style:normal;font-weight:300"><?php echo esc_html_x( 'This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein\'s evocative imagery, Strand\'s groundbreaking modernist approach, and Kōno\'s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography.', 'Answer in the FAQs pattern', 'twentytwentyfive' ); ?></p>
 				<!-- /wp:paragraph -->
 			</details>
@@ -44,7 +44,7 @@
 			<!-- wp:details {"style":{"border":{"bottom":{"color":"var:preset|color|opacity-20","width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
 			<details class="wp-block-details" style="border-bottom-color:var(--wp--preset--color--opacity-20);border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--30);font-style:normal;font-weight:400">
 				<summary><?php echo esc_html_x( 'When will The Stories Book be released?', 'Question in the FAQs pattern', 'twentytwentyfive' ); ?></summary>
-				<!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+				<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
 				<p style="font-style:normal;font-weight:300"><?php echo esc_html_x( 'This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein\'s evocative imagery, Strand\'s groundbreaking modernist approach, and Kōno\'s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography.', 'Answer in the FAQs pattern', 'twentytwentyfive' ); ?></p>
 				<!-- /wp:paragraph -->
 			</details>
@@ -53,7 +53,7 @@
 			<!-- wp:details {"style":{"border":{"bottom":{"color":"var:preset|color|opacity-20","width":"1px"}},"spacing":{"padding":{"bottom":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
 			<details class="wp-block-details" style="border-bottom-color:var(--wp--preset--color--opacity-20);border-bottom-width:1px;padding-bottom:var(--wp--preset--spacing--30);font-style:normal;font-weight:400">
 				<summary><?php echo esc_html_x( 'Are signed copies available?', 'Question in the FAQs pattern', 'twentytwentyfive' ); ?></summary>
-				<!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+				<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
 				<p style="font-style:normal;font-weight:300"><?php echo esc_html_x( 'This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein\'s evocative imagery, Strand\'s groundbreaking modernist approach, and Kōno\'s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography.', 'Answer in the FAQs pattern', 'twentytwentyfive' ); ?></p>
 				<!-- /wp:paragraph -->
 			</details>
@@ -62,7 +62,7 @@
 			<!-- wp:details {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
 			<details class="wp-block-details" style="font-style:normal;font-weight:400">
 				<summary><?php echo esc_html_x( 'Will The Stories Book be available in other languages other that English?', 'Question in the FAQs pattern', 'twentytwentyfive' ); ?></summary>
-				<!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+				<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
 				<p style="font-style:normal;font-weight:300"><?php echo esc_html_x( 'This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein\'s evocative imagery, Strand\'s groundbreaking modernist approach, and Kōno\'s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography.', 'Answer in the FAQs pattern', 'twentytwentyfive' ); ?></p>
 				<!-- /wp:paragraph -->
 			</details>

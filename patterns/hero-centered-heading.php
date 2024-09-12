1f7f99adf3fb206142c0280bd0e21e2578e4b3cb
Kevin Taron <kevin.taron@gmail.com>
2024-09-12 19:33:28 +0200

    fixed header size with custom font size heading-1

--- patterns/hero-centered-heading.php
+++ patterns/hero-centered-heading.php
@@ -15,8 +15,8 @@
 <div class="wp-block-group alignfull" style="min-height:0vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--40);">
 	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group">
-		<!-- wp:heading {"fontSize": "xx-large", "textAlign":"center","level":2,"style":{"spacing":{"margin":{"right":"0","left":"0"},"padding":{"right":"0","left":"0"}}}} -->
-		<h2 class="wp-block-heading has-text-align-center has-xx-large-font-size" style="margin-right:0;margin-left:0;padding-right:0;padding-left:0;">Tell your story</h2>
+		<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"var(--wp--custom--font-size--heading-1)"},"spacing":{"margin":{"right":"0","left":"0"},"padding":{"right":"0","left":"0"}}}} -->
+		<h2 class="wp-block-heading has-text-align-center" style="font-size:var(--wp--custom--font-size--heading-1);margin-right:0;margin-left:0;padding-right:0;padding-left:0;">Tell your story</h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"align":"center"} -->

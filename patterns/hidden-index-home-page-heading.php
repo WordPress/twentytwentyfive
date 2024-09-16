e57d891eeb7dd713507a1f6c80486f2d6dac48a1
Utsav Patel <utsav.patel1512@outlook.com>
2024-09-16 23:56:12 +0530

    feature: created hidden pattern for adding i18n to blog text of home & index template

---
+++ patterns/hidden-index-home-page-heading.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Hidden index home page heading
+ * Slug: twentytwentyfive/hidden-index-home-page-heading
+ * Description: Hidden heading for the home page and index template.
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:heading {"textAlign":"left","level":1} -->
+<h1 class="wp-block-heading has-text-align-left"><?php esc_html_e( 'Blog', 'twentytwentyfive' ); ?></h1>
+<!-- /wp:heading -->

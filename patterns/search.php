1c2b7806e46b8b798d7308102525706c66abad2a
Bob Matyas <45246438+bobmatyas@users.noreply.github.com>
2024-09-17 16:14:45 -0700

    Add PHP pattern file for search

---
+++ patterns/search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Search
+ * Slug: twentytwentyfive/search
+ * Categories: search
+ * Description: Translatable search form
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+
+<!-- wp:search {"label":"<?php esc_html_e( 'Search', 'twentytwentyfive' ); ?>","showLabel":false,"buttonText":<?php echo esc_attr_x( 'Search', 'button label', 'twentytwentyfive' ); ?>","style":{"border":{"radius":"50px"}},"fontSize":"medium"} /-->

ee5013ae9f5ee6bfdbb18e6f8fce06fba505ede1
Alex Lende <alex+github.com@lende.xyz>
2024-09-17 16:02:17 -0700

    Add additionalArgs

--- theme-utils.mjs
+++ theme-utils.mjs
@@ -14,6 +14,7 @@ import chalk, { Chalk } from 'chalk';
 const commands = {
 	'escape-patterns': {
 		helpText: 'Escapes block patterns for pattern files.',
+		additionalArgs: '<array of theme slugs>',
 		run: ( args ) => escapePatterns( args[ 1 ].split( /[ ,]+/ ) ),
 	},
 	'validate-theme': {

8407cd404550f6b37fa3af2dddd94fe553f4f67e
Juan Aldasoro <juanfraa@gmail.com>
2024-09-19 15:57:04 +0200

    Indent.

--- patterns/footer.php
+++ patterns/footer.php
@@ -13,59 +13,74 @@
 
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:site-logo /-->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:site-logo /-->
 
-<!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-<div class="wp-block-group alignfull"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
-<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:site-title {"level":2} /-->
+		<!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+		<div class="wp-block-group alignfull">
+			<!-- wp:columns -->
+			<div class="wp-block-columns">
+				<!-- wp:column {"width":"66.66%"} -->
+				<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:site-title {"level":2} /-->
 
-<!-- wp:site-tagline /--></div>
-<!-- /wp:column -->
+				<!-- wp:site-tagline /-->
+			</div>
+			<!-- /wp:column -->
 
-<!-- wp:column {"width":"33.33%"} -->
-<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:spacer {"height":"var:preset|spacing|40","width":"0px","style":{"layout":[]}} -->
-<div style="height:var(--wp--preset--spacing--40);width:0px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
+			<!-- wp:column {"width":"33.33%"} -->
+			<div class="wp-block-column" style="flex-basis:33.33%">
+				<!-- wp:spacer {"height":"var:preset|spacing|40","width":"0px"} -->
+				<div style="height:var(--wp--preset--spacing--40);width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+				<!-- /wp:spacer -->
+			</div>
+			<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|80"}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"top","justifyContent":"space-between"}} -->
-<div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
-<!-- wp:navigation-link {"label":"Blog","url":"#"} /-->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|80"}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"top","justifyContent":"space-between"}} -->
+		<div class="wp-block-group">
+			<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+				<!-- wp:navigation-link {"label":"Blog","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"About","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"About","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"FAQs","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"FAQs","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"Authors","url":"#"} /-->
-<!-- /wp:navigation -->
+				<!-- wp:navigation-link {"label":"Authors","url":"#"} /-->
+			<!-- /wp:navigation -->
 
-<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
-<!-- wp:navigation-link {"label":"Events","url":"#"} /-->
+			<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+				<!-- wp:navigation-link {"label":"Events","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"Shop","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"Shop","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"Patterns","url":"#"} /-->
+				<!-- wp:navigation-link {"label":"Patterns","url":"#"} /-->
 
-<!-- wp:navigation-link {"label":"Themes","url":"#"} /-->
-<!-- /wp:navigation --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
+				<!-- wp:navigation-link {"label":"Themes","url":"#"} /-->
+			<!-- /wp:navigation -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 
-<!-- wp:spacer {"height":"var:preset|spacing|70"} -->
-<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+	<!-- wp:spacer {"height":"var:preset|spacing|70"} -->
+	<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
 
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignfull"><!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size">Twenty Twenty-Five</p>
-<!-- /wp:paragraph -->
+	<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignfull">
+		<!-- wp:paragraph {"fontSize":"small"} -->
+		<p class="has-small-font-size">Twenty Twenty-Five</p>
+		<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size">Designed with <a href="https://wordpress.org">WordPress</a></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div>
+		<!-- wp:paragraph {"fontSize":"small"} -->
+		<p class="has-small-font-size">Designed with <a href="https://wordpress.org">WordPress</a></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
 <!-- /wp:group -->

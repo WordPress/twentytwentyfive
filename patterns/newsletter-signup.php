8c2ef41831042550a3ea28a0e6cd869e7a8708c2
Juan Aldasoro <juanfraa@gmail.com>
2024-09-19 17:53:45 +0200

    Fix indentation.

--- patterns/newsletter-signup.php
+++ patterns/newsletter-signup.php
@@ -12,7 +12,8 @@
 
 ?>
 <!-- wp:cover {"overlayColor":"accent-1","isUserOverlayColor":true,"minHeight":460,"isDark":false,"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:460px"><span aria-hidden="true" class="wp-block-cover__background has-accent-1-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container">
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:460px"><span aria-hidden="true" class="wp-block-cover__background has-accent-1-background-color has-background-dim-100 has-background-dim"></span>
+<div class="wp-block-cover__inner-container">
 	<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
 	<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">Sign up to get daily stories</h2>
 	<!-- /wp:heading -->
@@ -22,10 +23,12 @@
 	<!-- /wp:paragraph -->
 
 	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-	<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center"} -->
-	<div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button">Subscribe</a></div>
-	<!-- /wp:button --></div>
-	<!-- /wp:buttons -->
+	<div class="wp-block-buttons">
+		<!-- wp:button {"textAlign":"center"} -->
+		<div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button">Subscribe</a></div>
+		<!-- /wp:button -->
 	</div>
+	<!-- /wp:buttons -->
+</div>
 </div>
 <!-- /wp:cover -->

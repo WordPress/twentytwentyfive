57f82574a2883cf3b1eda6d05a0a981b2247af61
Carolina Nymark <hi@themesbycarolina.com>
2024-09-21 09:13:05 +0200

    Hidden-sidebar.php: Apply style variation to heading

--- patterns/hidden-sidebar.php
+++ patterns/hidden-sidebar.php
@@ -10,8 +10,8 @@
  */
 
 ?>
-<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"uppercase","letterSpacing":"1.6px"}},"fontSize":"small"} -->
-<h2 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:1.6px;text-transform:uppercase"><?php esc_html_e( 'Other Posts', 'twentytwentyfive' ); ?></h2>
+<!-- wp:heading {"className":"is-style-uppercase"} -->
+<h2 class="wp-block-heading is-style-uppercase"><?php esc_html_e( 'Other Posts', 'twentytwentyfive' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:spacer {"height":"var:preset|spacing|40"} -->

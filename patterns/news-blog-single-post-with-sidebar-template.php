483024905b59dba646dcb3029982103a6cb58446
Juan Aldasoro <juanfraa@gmail.com>
2024-09-23 17:20:37 +0200

    Update patterns/news-blog-single-post-with-sidebar-template.php

--- patterns/news-blog-single-post-with-sidebar-template.php
+++ patterns/news-blog-single-post-with-sidebar-template.php
@@ -110,7 +110,7 @@
 		<div class="wp-block-column" style="padding-top:0;padding-bottom:0;flex-basis:65%">
 			<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"}} -->
 			<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-					<!-- wp:pattern {"slug":"twentytwentyfive/comments"} /-->
+				<!-- wp:pattern {"slug":"twentytwentyfive/comments"} /-->
 			</div>
 			<!-- /wp:group -->
 		</div>

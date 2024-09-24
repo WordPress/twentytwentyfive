4f81cb617845c883fd76bc698bc36fe743cc3f8e
Rich Tabor <hi@richtabor.com>
2024-09-24 16:17:43 -0400

    more

--- patterns/grid-videos.php
+++ patterns/grid-videos.php
@@ -11,46 +11,40 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|60"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
-	<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide">
-		<!-- wp:heading {"textAlign":"left","level":3,"align":"wide","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
-		<h3 class="wp-block-heading alignwide has-text-align-left">Explore the episodes</h3>
-		<!-- /wp:heading -->
+<!-- wp:group {"metadata":{"categories":["banner"],"patternName":"twentytwentyfive/grid-videos","name":"Video grid"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|60"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"left","level":3,"align":"wide","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<h3 class="wp-block-heading alignwide has-text-align-left">Explore the episodes</h3>
+<!-- /wp:heading -->
 
-		<!-- wp:paragraph {"className":"is-style-pill"} -->
-		<p class="is-style-pill">Podcast</p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
+<!-- wp:paragraph {"className":"is-style-pill"} -->
+<p class="is-style-pill">Podcast</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
 
-	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","minimumColumnWidth":"23rem"}} -->
-	<div class="wp-block-group alignwide">
-		<!-- wp:video -->
-		<figure class="wp-block-video"></figure>
-		<!-- /wp:video -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","minimumColumnWidth":"19rem"}} -->
+<div class="wp-block-group alignwide"><!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->
 
-		<!-- wp:video -->
-		<figure class="wp-block-video"></figure>
-		<!-- /wp:video -->
+<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->
 
-		<!-- wp:video -->
-		<figure class="wp-block-video"></figure>
-		<!-- /wp:video -->
+<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->
 
-		<!-- wp:video -->
-		<figure class="wp-block-video"></figure>
-		<!-- /wp:video -->
+<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->
 
-		<!-- wp:video -->
-		<figure class="wp-block-video"></figure>
-		<!-- /wp:video -->
+<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->
 
-		<!-- wp:video -->
-		<figure class="wp-block-video"></figure>
-		<!-- /wp:video -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->

543556412860d6e15b1adaf17a6df46724748a56
Rich Tabor <hi@richtabor.com>
2024-09-25 17:36:21 -0400

    Update page-landing-event.php

--- patterns/page-landing-event.php
+++ patterns/page-landing-event.php
@@ -19,9 +19,8 @@
 <div class="wp-block-group alignfull">
 	<!-- wp:pattern {"slug":"twentytwentyfive/hero-full-width-image"} /-->
 	<!-- wp:pattern {"slug":"twentytwentyfive/heading-and-paragraph-with-image"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfive/two-headings-and-a-list"} /-->
-	<!-- wp:pattern {"slug":"twentytwentyfive/text-faqs"} /-->
 	<!-- wp:pattern {"slug":"twentytwentyfive/banner-description-images-grid"} /-->
+	<!-- wp:pattern {"slug":"twentytwentyfive/text-faqs"} /-->
 	<!-- wp:pattern {"slug":"twentytwentyfive/contact-centered-social-link"} /-->
 </div>
 <!-- /wp:group -->

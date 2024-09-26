959f278bed485a1756ec044dbcfd02e85016bede
Juan Aldasoro <juanfraa@gmail.com>
2024-09-26 15:08:29 +0200

    Add business home & shop home.

---
+++ patterns/page-business-home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Business Homepage
+ * Slug: twentytwentyfive/page-business-home
+ * Categories: twentytwentyfive_page, posts, featured
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A business homepage pattern.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull">
+	<!-- wp:pattern {"slug":"twentytwentyfive/cta-centered-heading"} /-->
+	<!-- wp:pattern {"slug":"twentytwentyfive/overlap-images-and-paragraph"} /-->
+	<!-- wp:pattern {"slug":"twentytwentyfive/services-three-columns"} /-->
+	<!-- wp:pattern {"slug":"twentytwentyfive/testimonials-review-large-image-right"} /-->
+	<!-- wp:pattern {"slug":"twentytwentyfive/pricing-2-col"} /-->
+	<!-- wp:pattern {"slug":"twentytwentyfive/newsletter-sign-up"} /-->
+</div>
+<!-- /wp:group -->

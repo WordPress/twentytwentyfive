3bdb23dc445b80d0e7d93672ce2289e0c3cedadc
Rich Tabor <hi@richtabor.com>
2024-09-26 11:34:11 -0400

    Update newsletter-signup.php

--- patterns/newsletter-signup.php
+++ patterns/newsletter-signup.php
@@ -11,24 +11,22 @@
  */
 
 ?>
-<!-- wp:cover {"overlayColor":"accent-1","isUserOverlayColor":true,"minHeight":460,"isDark":false,"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:460px"><span aria-hidden="true" class="wp-block-cover__background has-accent-1-background-color has-background-dim-100 has-background-dim"></span>
-<div class="wp-block-cover__inner-container">
-	<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-	<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">Sign up to get daily stories</h2>
+<!-- wp:cover {"overlayColor":"accent-1","isUserOverlayColor":true,"minHeight":460,"isDark":false,"metadata":{"categories":["call-to-action"],"patternName":"twentytwentyfive/newsletter-sign-up","name":"Newsletter Sign Up"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","contentSize":"800px"}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:460px"><span aria-hidden="true" class="wp-block-cover__background has-accent-1-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","className":"is-style-default","fontSize":"xx-large"} -->
+	<h2 class="wp-block-heading has-text-align-center is-style-default has-xx-large-font-size">Sign up to get daily stories</h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:paragraph {"align":"center"} -->
-	<p class="has-text-align-center">Get access to a curated collection of moments in time featuring<br>photographs from historical relevance.</p>
+	<!-- wp:paragraph {"align":"center","className":"is-style-text-subtitle"} -->
+	<p class="has-text-align-center is-style-text-subtitle">Get access to a curated collection of moments in time featuring photographs from historical relevance.</p>
 	<!-- /wp:paragraph -->
 
+	<!-- wp:spacer {"height":"var:preset|spacing|30"} -->
+	<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+
 	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-	<div class="wp-block-buttons">
-		<!-- wp:button {"textAlign":"center"} -->
+	<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center"} -->
 		<div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button">Subscribe</a></div>
-		<!-- /wp:button -->
-	</div>
-	<!-- /wp:buttons -->
-</div>
-</div>
+	<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div></div>
 <!-- /wp:cover -->

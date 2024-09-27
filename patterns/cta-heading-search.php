cd476736d93c0b2c002dcf10a7ea5948383693ad
Carolina Nymark <hi@themesbycarolina.com>
2024-09-28 01:33:39 +0200

    Nit: start heading markup on new line

--- patterns/cta-heading-search.php
+++ patterns/cta-heading-search.php
@@ -13,7 +13,8 @@
 ?>
 <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-	<div class="wp-block-group alignwide"><!-- wp:heading {"fontSize":"xx-large"} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:heading {"fontSize":"xx-large"} -->
 		<h2 class="wp-block-heading has-xx-large-font-size"><?php esc_html_e( 'What are you looking for?', 'twentytwentyfive' ); ?></h2>
 		<!-- /wp:heading -->
 

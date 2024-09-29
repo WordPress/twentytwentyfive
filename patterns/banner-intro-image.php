f1986d6b12917d595eb75957535a41695244d33b
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-09-29 23:01:39 +0600

    update the first h2 to an h1 tag

--- patterns/banner-intro-image.php
+++ patterns/banner-intro-image.php
@@ -27,8 +27,8 @@
 
 		<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
 		<div class="wp-block-column is-vertically-aligned-center">
-			<!-- wp:heading -->
-			<h2 class="wp-block-heading"><?php echo esc_html_x( 'New arrivals', 'Heading for banner with flower', 'twentytwentyfive' ); ?></h2>
+			<!-- wp:heading {"level":1,"fontSize":"x-large"} -->
+			<h2 class="wp-block-heading has-x-large-font-size"><?php echo esc_html_x( 'New arrivals', 'Heading for banner with flower', 'twentytwentyfive' ); ?></h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->

961114452060ed8ed32371560a6398b305a2b056
Troy Chaplin <troy.chaplin@gmail.com>
2024-10-07 07:56:01 -0400

    fix: incorrect heading level

--- patterns/banner-about-book.php
+++ patterns/banner-about-book.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Banner with book description
  * Slug: twentytwentyfive/banner-about-book
@@ -19,11 +20,11 @@
 		<!-- wp:column {"verticalAlignment":"center","width":""} -->
 		<div class="wp-block-column is-vertically-aligned-center">
 			<!-- wp:heading {"level":3,"className":"wp-block-heading","fontSize":"xx-large"} -->
-			<h3 class="wp-block-heading has-xx-large-font-size"><?php esc_html_e( 'About the book', 'twentytwentyfive' ); ?></h3>
+			<h2 class="wp-block-heading has-xx-large-font-size"><?php esc_html_e('About the book', 'twentytwentyfive'); ?></h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph {"fontSize":"medium"} -->
-			<p class="has-medium-font-size"><?php echo esc_html_x( 'This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein&rsquo;s evocative imagery, Strand&rsquo;s groundbreaking modernist approach, and Kōno&rsquo;s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography. Each image in &ldquo;The Stories Book&rdquo; is accompanied by insightful commentary, providing historical context and revealing the stories behind the photographs. This collection is not only a visual feast but also a tribute to the power of photography to preserve and narrate the multifaceted experiences of humanity.', 'Pattern placeholder text.', 'twentytwentyfive' ); ?></p>
+			<p class="has-medium-font-size"><?php echo esc_html_x('This exquisite compilation showcases a diverse array of photographs that capture the essence of different eras and cultures, reflecting the unique styles and perspectives of each artist. Fleckenstein&rsquo;s evocative imagery, Strand&rsquo;s groundbreaking modernist approach, and Kōno&rsquo;s meticulous documentation of Japanese life come together in a harmonious blend that celebrates the art of photography. Each image in &ldquo;The Stories Book&rdquo; is accompanied by insightful commentary, providing historical context and revealing the stories behind the photographs. This collection is not only a visual feast but also a tribute to the power of photography to preserve and narrate the multifaceted experiences of humanity.', 'Pattern placeholder text.', 'twentytwentyfive'); ?></p>
 			<!-- /wp:paragraph -->
 		</div>
 		<!-- /wp:column -->
@@ -31,7 +32,7 @@
 		<!-- wp:column {"verticalAlignment":"center","width":"","layout":{"type":"default"}} -->
 		<div class="wp-block-column is-vertically-aligned-center">
 			<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/book-image-landing.webp" alt="<?php esc_attr_e( 'Image of a book', 'twentytwentyfive' ); ?>" style="aspect-ratio:1;object-fit:cover"/></figure>
+			<figure class="wp-block-image size-full"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/book-image-landing.webp" alt="<?php esc_attr_e('Image of a book', 'twentytwentyfive'); ?>" style="aspect-ratio:1;object-fit:cover" /></figure>
 			<!-- /wp:image -->
 		</div>
 		<!-- /wp:column -->
